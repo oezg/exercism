145cba080de58a86b49c777d093cc9d600a0b345
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-05 18:10:39 +0200

    implement interfaces

--- csharp/remote-control-competition/RemoteControlCompetition.cs
+++ csharp/remote-control-competition/RemoteControlCompetition.cs
@@ -2,8 +2,13 @@ using System;
 using System.Collections.Generic;
 
 // TODO implement the IRemoteControlCar interface
+public interface IRemoteControlCar
+{
+    int DistanceTravelled { get; }
+    void Drive();
+}
 
-public class ProductionRemoteControlCar
+public class ProductionRemoteControlCar : IRemoteControlCar, IComparable
 {
     public int DistanceTravelled { get; private set; }
     public int NumberOfVictories { get; set; }
@@ -12,9 +17,15 @@ public class ProductionRemoteControlCar
     {
         DistanceTravelled += 10;
     }
+
+    public int CompareTo(object obj)
+    {
+        var other = obj as ProductionRemoteControlCar;
+        return this.NumberOfVictories - other.NumberOfVictories;
+    }
 }
 
-public class ExperimentalRemoteControlCar
+public class ExperimentalRemoteControlCar : IRemoteControlCar
 {
     public int DistanceTravelled { get; private set; }
 
@@ -28,12 +39,13 @@ public static class TestTrack
 {
     public static void Race(IRemoteControlCar car)
     {
-        throw new NotImplementedException($"Please implement the (static) TestTrack.Race() method");
+        car.Drive();
     }
 
     public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1,
         ProductionRemoteControlCar prc2)
-    {
-        throw new NotImplementedException($"Please implement the (static) TestTrack.GetRankedCars() method");
-    }
+        => prc1.CompareTo(prc2) < 0 
+            ? new List<ProductionRemoteControlCar>() { null, prc2, prc1 } 
+            : new List<ProductionRemoteControlCar>() { null, prc1, prc2 };
+
 }
