45e867ba8d08da2050f145a7e60229187f27154e
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-06 21:49:51 +0200

    solve faceid2

---
+++ csharp/faceid-2/Faceid2.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+public class FacialFeatures
+{
+    public string EyeColor { get; }
+    public decimal PhiltrumWidth { get; }
+
+    public FacialFeatures(string eyeColor, decimal philtrumWidth)
+    {
+        EyeColor = eyeColor;
+        PhiltrumWidth = philtrumWidth;
+    }
+    
+    public override bool Equals(object obj) 
+        => obj is FacialFeatures other && (EyeColor == other.EyeColor && PhiltrumWidth == other.PhiltrumWidth);
+
+    public override int GetHashCode() 
+        => HashCode.Combine(EyeColor, PhiltrumWidth);
+    
+}
+
+public class Identity
+{
+    public string Email { get; }
+    public FacialFeatures FacialFeatures { get; }
+
+    public Identity(string email, FacialFeatures facialFeatures)
+    {
+        Email = email;
+        FacialFeatures = facialFeatures;
+    }
+
+    public override bool Equals(object obj)
+    => obj is Identity other && Email == other.Email && FacialFeatures.Equals(other.FacialFeatures);
+
+    public override int GetHashCode()
+        => HashCode.Combine(Email, FacialFeatures);
+}
+
+public class Authenticator
+{
+    private readonly HashSet<Identity> _identitySet = new();
+    private static readonly Identity Admin = new("admin@exerc.ism", new FacialFeatures("green", 0.9m));
+    
+    public static bool AreSameFace(FacialFeatures faceA, FacialFeatures faceB) => faceA.Equals(faceB);
+
+    public bool IsAdmin(Identity identity) => Admin.Equals(identity);
+
+    public bool Register(Identity identity)
+        => !IsRegistered(identity) && _identitySet.Add(identity);
+
+    public bool IsRegistered(Identity identity) => _identitySet.Contains(identity);
+
+    public static bool AreSameObject(Identity identityA, Identity identityB) => identityA == identityB;
+}
