cf00cb1a857d3ec535c717623bcb95cc940b0a28
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-10 23:20:10 +0200

    solve space age

---
+++ csharp/space-age/unskip_tests.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+directory = sys.argv[1]
+
+
+files = os.listdir(directory)
+try:
+    test = [file for file in files if file.endswith("Tests.cs")][0]
+    with open(test, "r+") as test_file:
+        text = test_file.read()
+        unskipped_text = text.replace('(Skip = "Remove this Skip property to run this test")', '')
+        test_file.seek(0)
+        test_file.write(unskipped_text)
+        test_file.truncate()
+except:
+    pass
+finally:
+    print(directory)
