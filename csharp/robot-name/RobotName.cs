442c4f9298df84fd14f0d1a0efb577e58a0a443a
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-11 15:34:01 +0200

    solve robot name

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 
 public class Robot
 {
-    public string Name
+    private static HashSet<string> names = new HashSet<string>();
+
+    public string Name { get; private set; }
+    public Robot() 
     {
-        get
-        {
-            string name = "";
-            var random = new Random();
-            name += (char)(random.Next('A', 'Z' + 1));
-            name += (char)(random.Next('A', 'Z' + 1));
-            name += random.Next(10);
-            name += random.Next(10);
-            name += random.Next(10);
-            return name;
-        }
+        Reset();
     }
 
     public void Reset()
     {
-        Name = Name;
+        Name = GenerateNewName();
+    }
+
+    private static string GenerateName()
+    {
+        string name = "";
+        var random = new Random();
+        name += (char)(random.Next('A', 'Z' + 1));
+        name += (char)(random.Next('A', 'Z' + 1));
+        name += random.Next(10);
+        name += random.Next(10);
+        name += random.Next(10);
+        return name;
+    }
+
+    private static string GenerateNewName()
+    {
+        string name = GenerateName();
+        while (names.Contains(name))
+        {
+            name = GenerateName();
+        }
+        names.Add(name);
+        return name;
     }
 }
