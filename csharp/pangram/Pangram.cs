ad86f05ed4cfe6b81417dc0dbe6c8e713ed3eb3e
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-16 18:38:39 +0200

    refactor TrueForAll

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -4,7 +4,6 @@ using System.Linq;
 public static class Pangram
 {
     public static bool IsPangram(string input)
-    {
-        return "abcdefghijklmnopqrstuvwxyz".ToList().Aggregate(true, (acc, curr) => acc && input.Contains(curr, StringComparison.OrdinalIgnoreCase));
-    }
+        => "abcdefghijklmnopqrstuvwxyz".ToList()
+        .TrueForAll(ch => input.Contains(ch, StringComparison.OrdinalIgnoreCase));
 }
