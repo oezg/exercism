ef497c98adc8c65068c3379f7e5121fd195fd627
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-17 12:47:27 +0200

    solve grains, Range and Aggregate

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+
+public static class Grains
+{
+    public static ulong Square(int n) 
+        => n > 0 && n < 65 ? (ulong)Math.Pow(2, n - 1) 
+        : throw new ArgumentOutOfRangeException(nameof(n), n.ToString());
+
+    public static ulong Total() 
+        => Enumerable.Range(1, 64).Aggregate(0UL, (acc, curr) => acc + Square(curr));
+}
