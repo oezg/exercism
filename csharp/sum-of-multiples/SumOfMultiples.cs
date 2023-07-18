452132787ed378a5b9706b20514059c90b4b7f44
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-18 22:46:31 +0200

    refactor LINQ

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -5,15 +5,7 @@ using System.Linq;
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
-    {
-        HashSet<int> result = new HashSet<int>();
-        foreach (int multiple in multiples)
-        {
-            for (int i = 1; multiple > 0 && i * multiple < max; i++)
-            {
-                result.Add(i * multiple);
-            }
-        }
-        return result.Sum();
-    }
+        => Enumerable.Range(0, max)
+        .Where(n => multiples.Any(x => x > 0 && n % x == 0))
+        .Sum();
 }
