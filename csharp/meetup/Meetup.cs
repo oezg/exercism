b39a9cbd61ec3dfd67e7e3c733923f8609b02e38
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-19 14:29:21 +0200

    const WeeKlength

--- csharp/meetup/Meetup.cs
+++ csharp/meetup/Meetup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public enum Schedule
 {
@@ -13,31 +14,35 @@ public enum Schedule
 
 public class Meetup
 {
+    private const int WeekLength = 7;
+
     public int Month { get; set; }
 
     public int Year { get; set; }
+
+    public IEnumerable<DateTime> Dates { get; }
 
     public Meetup(int month, int year)
     {
         Month = month;
         Year = year;
+        Dates = Enumerable.Range(1, DateTime.DaysInMonth(Year, Month)).Select(dayNumber => new DateTime(Year, Month, dayNumber));
     }
 
     public DateTime Day(DayOfWeek dayOfWeek, Schedule schedule) => schedule switch
     {
-        Schedule.Teenth => firstOccurrenceAfterOffset(dayOfWeek, 13),
-        Schedule.First => firstOccurrenceAfterOffset(dayOfWeek),
-        Schedule.Second => firstOccurrenceAfterOffset(dayOfWeek, 8),
-        Schedule.Third => firstOccurrenceAfterOffset(dayOfWeek, 15),
-        Schedule.Fourth => firstOccurrenceAfterOffset(dayOfWeek, 22),
-        Schedule.Last => firstOccurrenceAfterOffset(dayOfWeek, DateTime.DaysInMonth(Year, Month) - 6),
+        Schedule.Teenth => firstOccurrenceAfterOffset(dayOfWeek, 12),
+        Schedule.First => firstOccurrenceAfterOffset(dayOfWeek, 0 * WeekLength),
+        Schedule.Second => firstOccurrenceAfterOffset(dayOfWeek, 1 * WeekLength),
+        Schedule.Third => firstOccurrenceAfterOffset(dayOfWeek, 2 * WeekLength),
+        Schedule.Fourth => firstOccurrenceAfterOffset(dayOfWeek, 3 * WeekLength),
+        Schedule.Last => firstOccurrenceAfterOffset(dayOfWeek, DateTime.DaysInMonth(Year, Month) - WeekLength),
         _ => throw new ArgumentException(),
     };
 
-    private DateTime firstOccurrenceAfterOffset(DayOfWeek dayOfWeek, int offset = 1)
+    private DateTime firstOccurrenceAfterOffset(DayOfWeek dayOfWeek, int offset = 0)
     {
-        DateTime dt = new DateTime(Year, Month, offset);
-        dt = dt.AddDays(((dayOfWeek - dt.DayOfWeek) % 7 + 7) % 7);
-        return dt;
+        DateTime dt = new DateTime(Year, Month, offset + 1);
+        return dt.AddDays(((dayOfWeek - dt.DayOfWeek) % WeekLength + WeekLength) % WeekLength);
     }
 }
