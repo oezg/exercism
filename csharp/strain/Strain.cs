dd8ddcbb3eadfcaa25fd4cb4cab9a640aa5abe36
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-19 23:52:38 +0200

    refactor strain, reuse keep

--- csharp/strain/Strain.cs
+++ csharp/strain/Strain.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public static class Strain
 {
-    public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+    public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Predicate<T> predicate)
     {
         foreach (var item in collection)
         {
@@ -14,14 +14,6 @@ public static class Strain
         }
     }
 
-    public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
-    {
-        foreach (var item in collection)
-        {
-            if (!predicate(item))
-            {
-                yield return item;
-            }
-        }
-    }
+    public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Predicate<T> predicate)
+        => collection.Keep(item => !predicate(item));
 }
