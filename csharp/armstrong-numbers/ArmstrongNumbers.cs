fd8d1e72e415dfebd7df1f3862d9b39712ae289f
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-19 15:12:11 +0200

    refactor armstrong, yield return ienumerable

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -1,13 +1,22 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
+
+using Newtonsoft.Json.Linq;
 
 public static class ArmstrongNumbers
 {
-    public static bool IsArmstrongNumber(int number) 
-        => number == number
-            .ToString()
-            .ToList()
-            .Select(x => x - '0')
-            .Select(x => Math.Pow(x, number.ToString().Length))
-            .Sum();
+    public static bool IsArmstrongNumber(int number)
+        => number == number.Digits().Sum(n => Math.Pow(n, number.Digits().Count()));
+
+    public static IEnumerable<int> Digits(this int n)
+    {
+        while (n > 0)
+        {
+            yield return n % 10;
+            n /= 10;
+        }
+    }
 }
