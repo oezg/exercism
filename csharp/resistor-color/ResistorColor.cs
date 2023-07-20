c544190d4c31f085a71544876206bc182f8c921a
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-20 13:17:54 +0200

    rename s_colors, private static member

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -2,8 +2,8 @@ using System;
 
 public static class ResistorColor
 {
-    private static readonly string[] _colors = new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
-    public static int ColorCode(string color) => Array.IndexOf(_colors, color);
+    private static readonly string[] s_colors = new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+    public static int ColorCode(string color) => Array.IndexOf(s_colors, color);
 
-    public static string[] Colors() => _colors;
+    public static string[] Colors() => s_colors;
 }
