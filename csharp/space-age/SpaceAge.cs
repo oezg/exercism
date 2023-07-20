6f1e4fc05ec540d04568bc816ac6db41636bf61a
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-20 13:37:48 +0200

    refactor space age, orbital period

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -4,21 +4,25 @@ public class SpaceAge
 {
     private readonly int _seconds;
 
+    private const int _earthYearInSeconds = 31_557_600;
+
     public SpaceAge(int seconds) => _seconds = seconds;
 
-    public double OnEarth() => _seconds / 31557600.0;
+    public double OnEarth() => (double)_seconds / _earthYearInSeconds;
 
-    public double OnMercury() => OnEarth() / 0.2408467;
+    public double OrbitalPeriod(double ratio) => OnEarth() / ratio;
 
-    public double OnVenus() => OnEarth() / 0.61519726;
+    public double OnMercury() => OrbitalPeriod(0.2408467);
+
+    public double OnVenus() => OrbitalPeriod(0.61519726);
     
-    public double OnMars() => OnEarth() / 1.8808158;
+    public double OnMars() => OrbitalPeriod(1.8808158);
 
-    public double OnJupiter() => OnEarth() / 11.862615;
+    public double OnJupiter() => OrbitalPeriod(11.862615);
 
-    public double OnSaturn() => OnEarth() / 29.447498;
+    public double OnSaturn() => OrbitalPeriod(29.447498);
 
-    public double OnUranus() => OnEarth() / 84.016846;
+    public double OnUranus() => OrbitalPeriod(84.016846);
 
-    public double OnNeptune() => OnEarth() / 164.79132;
+    public double OnNeptune() => OrbitalPeriod(164.79132);
 }
