76ea65747e7786799017c75503be04700bcecb15
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-21 12:11:05 +0200

    first solve binary search, while loop

--- csharp/binary-search/BinarySearch.cs
+++ csharp/binary-search/BinarySearch.cs
@@ -4,6 +4,25 @@ public static class BinarySearch
 {
     public static int Find(int[] input, int value)
     {
-        int index = -1;
+        int upper = input.Length;
+        int lower = 0;
+        int middle = upper / 2;
+        while(lower < upper)
+        {
+            middle = lower + (upper - lower) / 2;
+            if (input[middle] == value)
+            {
+                return middle;
+            }
+            else if (input[middle] > value)
+            {
+                upper = middle;
+            }
+            else
+            {
+                lower = middle + 1;
+            }
+        }
+        return -1;
     }
 }
