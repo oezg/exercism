75297085488c2728236b2486e5f66b04429de94e
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-07-27 13:25:16 +0200

    refactor grade school, struct Student

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -3,33 +3,43 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 
+public struct Student : IEquatable<Student>, IComparable<Student>
+{
+    public string Name;
+    public int Grade;
+
+    public Student(string name, int grade) : this()
+    {
+        Name = name;
+        Grade = grade;
+    }
+
+    public int CompareTo(Student other) 
+        => Grade == other.Grade 
+        ? Name.CompareTo(other.Name) 
+        : Grade - other.Grade;
+
+    public bool Equals(Student other) => Name == other.Name;
+}
+
 public class GradeSchool
 {
-    public Dictionary<int, HashSet<string>> Grades { get; } = new();
+    private List<Student> _students { get; } = new();
 
-    public bool Add(string student, int grade)
+    public bool Add(string name, int grade)
     {
-        if (Roster().Contains(student))
+        Student student = new Student(name, grade);
+        if (_students.Contains(student))
         {
             return false;
         }
-        if (!Grades.ContainsKey(grade))
-        {
-            Grades[grade] = new HashSet<string>();
-        }
-        return Grades[grade].Add(student);
+        _students.Add(student);
+        return true;
     }
 
     public IEnumerable<string> Roster()
-    {
-        List<string> roster = new List<string>();
-        foreach ( var key in Grades.Keys.ToImmutableSortedSet())
-        {
-            roster.AddRange(Grades[key].ToImmutableSortedSet());
-        }
-        return roster;
-    }
+        => _students.Order().Select(s => s.Name);
 
-    public IEnumerable<string> Grade(int grade) => 
-        Grades.GetValueOrDefault(grade, new HashSet<string>()).ToImmutableSortedSet();
+    public IEnumerable<string> Grade(int grade) 
+        => _students.Where(s => s.Grade == grade).Select(s => s.Name).Order();
 }
