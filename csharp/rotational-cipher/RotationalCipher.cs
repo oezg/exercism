52c6a77e645dc77c8621f687b301f788bd910910
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-08-02 10:52:58 +0200

    rotational cipher aggregate instead of string join

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -5,10 +5,10 @@ using System.Text;
 public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
-        => string.Join("", text.Select(chr => chr switch
+        => text.Aggregate("", (cipher, chr) => cipher + chr switch
         {
             <= 'z' and >= 'a' => (char)('a' + ((chr - 'a' + shiftKey) % ('z' - 'a' + 1))),
             <= 'Z' and >= 'A' => (char)('A' + ((chr - 'A' + shiftKey) % ('Z' - 'A' + 1))),
             _ => chr
-        })).ToString();
+        });
 }
