7cce929b1d30f225d9252377a023ec3c12d40fe3
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-08-02 10:40:37 +0200

    rotational cipher with string builder appendjoin

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -5,19 +5,10 @@ using System.Text;
 public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
-    {
-        StringBuilder stringBuilder = new StringBuilder();
-        foreach (char chr in text)
+        => new StringBuilder().AppendJoin("", text.Select(chr => chr switch
         {
-            _ = stringBuilder.Append(
-                chr switch
-                {
-                    <= 'z' and >= 'a' => (char)('a' + ((chr - 'a' + shiftKey) % 26)),
-                    <= 'Z' and >= 'A' => (char)('A' + ((chr - 'A' + shiftKey) % 26)),
-                    _ => chr
-                }
-                );
-        }
-        return stringBuilder.ToString();
-    }
+            <= 'z' and >= 'a' => (char)('a' + ((chr - 'a' + shiftKey) % 26)),
+            <= 'Z' and >= 'A' => (char)('A' + ((chr - 'A' + shiftKey) % 26)),
+            _ => chr
+        })).ToString();
 }
