403aa59cdbcb95d0f9e3b6fa8c9202095de79560
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-08-07 12:38:52 +0200

    throws exceptions separated

--- csharp/all-your-base/AllYourBase.cs
+++ csharp/all-your-base/AllYourBase.cs
@@ -6,12 +6,15 @@ public static class AllYourBase
 {
     public static int[] Rebase(int inputBase, int[] inputDigits, int outputBase)
     {
-        var result = 0;
-        var numDigits = inputDigits.Length;
-        for (int i = 0; i < numDigits; i++)
-        {
-            result += inputDigits[i] * (int)(Math.Pow(inputBase, numDigits - 1 - i));
-        }
+        ThrowExceptions(inputBase, inputDigits, outputBase);
+
+        var result = Enumerable.Range(0, inputDigits.Length)
+            .Zip(inputDigits)
+            .Select(pair => pair.Second * (int)Math.Pow(inputBase, inputDigits.Length - 1 - pair.First))
+            .Sum();
+        
+        if (result == 0) return new int[1] {0};
+        
         var output = new LinkedList<int>();
         while (result > 0)
         {
@@ -19,5 +22,13 @@ public static class AllYourBase
             result /= outputBase;
         }
         return output.ToArray();
+    }
+
+    public static void ThrowExceptions(int inputBase, int[] inputDigits, int outputBase)
+    {
+        if (inputBase < 2 || outputBase < 2) 
+            throw new ArgumentException("base can not be less than 2");
+        if (inputDigits.Any(digit => digit < 0 || digit >= inputBase))
+            throw new ArgumentException($"one of digits is not between 0 and {inputBase}");
     }
 }
