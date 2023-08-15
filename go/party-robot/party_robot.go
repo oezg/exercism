ad5295bd491e5ff32c9d5ac2deb9800f4565d2d3
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-08-15 13:52:13 +0200

    party robot

--- go/party-robot/party_robot.go
+++ go/party-robot/party_robot.go
@@ -1,16 +1,22 @@
+// Package partyrobot greets people and helps them to their seats.
 package partyrobot
+
+import "fmt"
 
 // Welcome greets a person by name.
 func Welcome(name string) string {
-	panic("Please implement the Welcome function")
+	return fmt.Sprintf("Welcome to my party, %s!", name)
 }
 
 // HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
 func HappyBirthday(name string, age int) string {
-	panic("Please implement the HappyBirthday function")
+	return fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
 }
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	panic("Please implement the AssignTable function")
+	var format = "Welcome to my party, %s!\n"
+	format += "You have been assigned to table %03d. Your table is %s, exactly %.1f meters from here.\n"
+	format += "You will be sitting next to %s."
+	return fmt.Sprintf(format, name, table, direction, distance, neighbor)
 }
