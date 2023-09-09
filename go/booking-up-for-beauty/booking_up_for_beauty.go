96ff7d2a5e13ea9ea4f0586269b53b1d1cc462fa
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-09-10 01:07:11 +0200

    refactor schedule for all layouts

--- go/booking-up-for-beauty/booking_up_for_beauty.go
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -1,45 +1,39 @@
 package booking
 
 import (
-	"fmt"
-	"log"
 	"time"
 )
 
 // Schedule returns a time.Time from a string containing a date.
 func Schedule(date string) time.Time {
-	t, err := time.Parse("1/2/2006 15:4:5", date)
-	if err != nil {
-		log.Fatal(err)
+	layouts := []string{
+		"1/2/2006 15:4:5",
+		"January 2, 2006 15:4:5",
+		"Monday, January 2, 2006 15:4:5",
+		"1/2/2006 15:4:5",
 	}
-	return t
+	for _, layout := range layouts {
+		t, err := time.Parse(layout, date)
+		if err == nil {
+			return t
+		}
+	}
+	return time.Time{}
 }
 
 // HasPassed returns whether a date has passed.
 func HasPassed(date string) bool {
-	t, err := time.Parse("January 2, 2006 15:4:5", date)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return t.Before(time.Now())
+	return Schedule(date).Before(time.Now())
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
 func IsAfternoonAppointment(date string) bool {
-	t, err := time.Parse("Monday, January 2, 2006 15:4:5", date)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return t.Hour() >= 12 && t.Hour() < 18
+	return Schedule(date).Hour() >= 12 && Schedule(date).Hour() < 18
 }
 
 // Description returns a formatted string of the appointment time.
 func Description(date string) string {
-	t, err := time.Parse("1/2/2006 15:4:5", date)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return fmt.Sprintf("You have an appointment on %s.", t.Format("Monday, January 2, 2006, at 15:4"))
+	return Schedule(date).Format("You have an appointment on Monday, January 2, 2006, at 15:4.")
 }
 
 // AnniversaryDate returns a Time with this year's anniversary.
