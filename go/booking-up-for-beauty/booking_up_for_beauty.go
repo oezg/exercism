fc2431056807a2fc3688ceaf7b01d05f13d62bcd
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-09-09 23:46:05 +0200

    time

--- go/booking-up-for-beauty/booking_up_for_beauty.go
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -1,28 +1,48 @@
 package booking
 
-import "time"
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
 // Schedule returns a time.Time from a string containing a date.
 func Schedule(date string) time.Time {
-	panic("Please implement the Schedule function")
+	t, err := time.Parse("1/2/2006 15:4:5", date)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return t
 }
 
 // HasPassed returns whether a date has passed.
 func HasPassed(date string) bool {
-	panic("Please implement the HasPassed function")
+	t, err := time.Parse("January 2, 2006 15:4:5", date)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return t.Before(time.Now())
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
 func IsAfternoonAppointment(date string) bool {
-	panic("Please implement the IsAfternoonAppointment function")
+	t, err := time.Parse("Monday, January 2, 2006 15:4:5", date)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return t.Hour() >= 12 && t.Hour() < 18
 }
 
 // Description returns a formatted string of the appointment time.
 func Description(date string) string {
-	panic("Please implement the Description function")
+	t, err := time.Parse("1/2/2006 15:4:5", date)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("You have an appointment on %s.", t.Format("Monday, January 2, 2006, at 15:4"))
 }
 
 // AnniversaryDate returns a Time with this year's anniversary.
 func AnniversaryDate() time.Time {
-	panic("Please implement the AnniversaryDate function")
+	return time.Date(time.Now().Year(), time.September, 15, 0, 0, 0, 0, time.UTC)
 }
---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module exercism/go
+
+go 1.21.0
