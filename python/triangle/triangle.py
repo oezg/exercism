20f251b68e7f41758cc2041d7a9e8fb2814219bc
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-09-15 00:11:57 +0200

    triangle

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,16 @@
-def equilateral(sides):
-    pass
+def equilateral(sides: list[int]) -> bool:
+    a, b, c = sides
+    return a > 0 and a == b and b == c
 
 
-def isosceles(sides):
-    pass
+def isosceles(sides: list[int]) -> bool:
+    return triangle(sides) and any(sides[i] == sides[(i+1) % len(sides)] for i in range(len(sides)))
 
 
-def scalene(sides):
-    pass
+def scalene(sides: list[int]) -> bool:
+    return triangle(sides) and all(sides[i] != sides[(i+1) % len(sides)] for i in range(len(sides)))
+
+
+def triangle(sides: list[int]) -> bool:
+    a, b, c = sides
+    return all(sides) and a + b >= c and b + c >= a and a + c >= b
