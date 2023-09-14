cae8683bb1828c378120bd84bf40028fbf120275
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-09-15 00:19:34 +0200

    grains on chessboard

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,8 @@
-def square(number):
-    pass
+def square(number: int) -> int:
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+    return 2 ** (number - 1)
 
 
-def total():
-    pass
+def total() -> int:
+    return sum(square(number) for number in range(1, 65))
