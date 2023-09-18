91d23c9dfddf4127daf8710327ed2a3ee1eba16e
Özgür Yıldırım <oezguer.yildirim@swr.de>
2023-09-18 11:59:08 +0200

    restore 3 solutions reverse string

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -1,8 +1,21 @@
 using System;
+using System.Linq;
 
 public static class ReverseString
 {
     public static string Reverse(string input)
+    {
+        var letters = input.ToCharArray();
+        Array.Reverse(letters);
+        return new string(letters);
+    }
+
+
+    public static string Reverse2(string input)
+        => new(input.Reverse().ToArray());
+
+
+    public static string Reverse1(string input)
     {
         var result = new char[input.Length];
         for (int i = 0; i < input.Length; i++)
