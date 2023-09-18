6ede76609d27127c7ab25eb0748d79a27d559e95
Özgür Yıldırım <oezguer.yildirim@swr.de>
2023-09-18 15:34:51 +0200

    yield break

---
+++ csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public static class PythagoreanTriplet
+{
+    public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
+    {
+        if (sum < 12)
+        {
+            yield break;
+        }
+
+    }
+}
