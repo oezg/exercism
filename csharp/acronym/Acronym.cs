f9f48e80a7853b2c87781da622060d362555688a
Özgür Yıldırım <oezguer.yildirim@swr.de>
2023-09-18 14:49:35 +0200

    aggregate abbreviate

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -5,6 +5,11 @@ using System.Text.RegularExpressions;
 public static class Acronym
 {
     public static string Abbreviate(string phrase)
+        => Regex.Matches(phrase, @"([A-Za-z']+)")
+        .Aggregate("", (a, c) => a + char.ToUpper(c.Groups[1].Value[0]));
+
+
+    public static string Abbreviate1(string phrase)
         => string.Join(
             "", 
             Regex.Matches(phrase, @"[\w\p{Po}]*?([A-Za-z])[\w\p{Po}]*")
