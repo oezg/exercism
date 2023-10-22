26a0895da8c2a659e6e2dee80df41e6eeb082828
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-10-23 01:07:48 +0200

    darts

---
+++ python/pangram/pangram.py
@@ -0,0 +1,15 @@
+"""
+pangram provides utility to figure out if a sentence is a pangram.
+"""
+
+from string import ascii_lowercase
+
+
+def is_pangram(sentence: str) -> bool:
+    """
+    A pangram is a sentence using every letter of the alphabet at least once.
+    It is case insensitive.
+    A pangram contains each of the 26 letters in the English alphabet.
+    """
+
+    return len(set(ascii_lowercase).difference(sentence.lower())) == 0
--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -1,2 +1,11 @@
-def reverse(text):
-    pass
+"""
+reverse_string provides a utility to reverse a string
+"""
+
+
+def reverse(text: str) -> str:
+    """
+    returns its input string reversed
+    """
+
+    return text[::-1]
