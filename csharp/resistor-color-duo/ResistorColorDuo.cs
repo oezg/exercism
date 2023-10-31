694f1d262775f14aaf487cf4e05619cf426cd204
Özgür Yıldırım <oezguer.yildirim@swr.de>
2023-10-31 15:08:40 +0100

    solve roman numerals

--- csharp/resistor-color-duo/ResistorColorDuo.cs
+++ csharp/resistor-color-duo/ResistorColorDuo.cs
@@ -1,19 +1,21 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public static class ResistorColorDuo
 {
     private enum Color
     {
-        Black, Brown, Red, Orange, Yellow, Green, Blue, Violet, Grey, White,
+        black, brown, red, orange, yellow, green, blue, violet, grey, white,
     }
 
+    private static readonly Dictionary<string, int> _colorValues;
+
+    static ResistorColorDuo() 
+        => _colorValues = ((Color[])Enum.GetValues(typeof(Color)))
+        .ToDictionary(color => color.ToString(), color => (int)color);
+
     public static int Value(string[] colors)
-        => 10 * calculateValue(colors[0]) + calculateValue(colors[1]);
+        => 10 * _colorValues[colors[0]] + _colorValues[colors[1]];
 
-    private static int calculateValue(string color)
-        => Enum.TryParse(color.Capitalize(), out Color c) ? (int)c : 0;
-
-    private static string Capitalize(this string str)
-        => string.IsNullOrEmpty(str) ? str : char.ToUpper(str[0]) + str[1..];
 }
