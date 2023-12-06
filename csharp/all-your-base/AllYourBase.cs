d6b63992a1d7a354d8a58272afcd1d027306a3f7
Özgür Yıldırım <oezguer.yildirim@swr.de>
2023-12-06 12:43:10 +0100

    refactor all your base

--- csharp/all-your-base/AllYourBase.cs
+++ csharp/all-your-base/AllYourBase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 public static class AllYourBase
@@ -7,24 +8,32 @@ public static class AllYourBase
     public static int[] Rebase(int inputBase, int[] inputDigits, int outputBase)
     {
         ThrowExceptions(inputBase, inputDigits, outputBase);
-
-        var result = Enumerable.Range(0, inputDigits.Length)
-            .Zip(inputDigits)
-            .Select(pair => pair.Second * (int)Math.Pow(inputBase, inputDigits.Length - 1 - pair.First))
-            .Sum();
-        
-        if (result == 0) return new int[1] {0};
-        
-        var output = new LinkedList<int>();
-        while (result > 0)
-        {
-            output.AddFirst(result % outputBase);
-            result /= outputBase;
-        }
-        return output.ToArray();
+        return ConvertToBaseArray(outputBase)(ConvertToBase10Value(inputDigits)(inputBase));
     }
 
-    public static void ThrowExceptions(int inputBase, int[] inputDigits, int outputBase)
+    private static Func<int, int> ConvertToBase10Value(int[] inputDigits) 
+        => inputBase => inputDigits
+        .Select((digit, index) => digit * (int)Math.Pow(inputBase, inputDigits.Length - 1 - index))
+        .Sum();
+
+    private static Func<int, int[]> ConvertToBaseArray(int outputBase)
+        => base10Value =>
+        {
+            if (base10Value == 0)
+            {
+                return new int[] { default };
+            }
+
+            var output = new LinkedList<int>();
+            while (base10Value > 0)
+            {
+                output.AddFirst(base10Value % outputBase);
+                base10Value /= outputBase;
+            }
+            return output.ToArray();
+        };
+
+    private static void ThrowExceptions(int inputBase, int[] inputDigits, int outputBase)
     {
         if (inputBase < 2 || outputBase < 2) 
             throw new ArgumentException("base can not be less than 2");
