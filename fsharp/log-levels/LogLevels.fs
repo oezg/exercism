28bc484348115edf79541426165a68025d8e7568
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-01-23 22:41:59 +0100

    fsharp

--- fsharp/log-levels/LogLevels.fs
+++ fsharp/log-levels/LogLevels.fs
@@ -1,7 +1,13 @@
 module LogLevels
 
-let message (logLine: string): string = failwith "Please implement the 'message' function"
+let message (logLine: string): string =
+    logLine.Split(':').[1].Trim()
 
-let logLevel(logLine: string): string = failwith "Please implement the 'logLevel' function"
+let logLevel(logLine: string): string = 
+    let start = logLine.IndexOf('[') + 1
+    let ende = logLine.IndexOf(']') - 1
+    let mitte = logLine[start..ende]
+    mitte.ToLower()
 
-let reformat(logLine: string): string = failwith "Please implement the 'reformat' function"
+let reformat(logLine: string): string = 
+    sprintf "%s (%s)" (message logLine) (logLevel logLine)
