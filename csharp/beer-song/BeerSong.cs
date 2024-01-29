6a150e957b5522876ca597049e93eeffeeb1dc56
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-01-30 00:38:38 +0100

    refactor beer song c#

--- csharp/beer-song/BeerSong.cs
+++ csharp/beer-song/BeerSong.cs
@@ -3,9 +3,43 @@ using System.Linq;
 
 public static class BeerSong
 {
-    public static string Recite(int startBottles, int takeDown)
-        => Enumerable.Range(startBottles - takeDown + 1, takeDown).Reverse()
-        .Select(i => @$"{(i == 0 ? "No more" : i)} bottle{(i != 1 ? "s" : "")} of beer on the wall, {(i == 0 ? "no more" : i)} bottle{(i != 1 ? "s" : "")} of beer.
-{(i == 0 ? "Go to the store and buy some more" : $"Take {(i == 1 ? "it" : "one")} down and pass it around")}, {(i == 1 ? "no more" : i == 0 ? "99" : i - 1)} bottle{(i != 2 ? "s" : "")} of beer on the wall.")
-        .Aggregate((a, b) => a + "\n\n" + b);
+    public static string Recite(int startBottles, int takeDown)       
+        => string.Join("\n\n",
+            Enumerable.Range(startBottles - takeDown + 1, takeDown)
+            .Reverse()
+            .Select(formatStrophe)
+        );
+
+    private static string formatStrophe(int count)
+        => string.Join('\n',
+            firstLine(bottles(count)),
+            secondLine(takeOrBuy(count), bottles(count - 1))
+        );
+
+    private static string firstLine(string bottles)
+        => $"{capitalize(bottles)} on the wall, {bottles}.";
+
+    private static string secondLine(string takeOrBuy, string bottles)
+        => $"{takeOrBuy}, {bottles} on the wall.";
+
+    private static string bottles(int count)
+        => $"{howMany(count)} {beer(count)}";
+
+    private static string beer(int count)
+        => $"bottle{s(count)} of beer";
+
+    private static string howMany(int count) 
+        => count == 0 ? "no more" : ((count % 100 + 100) % 100).ToString();
+
+    private static string s(int count)
+        => count == 1 ? "" : "s";
+
+    private static string takeOrBuy(int count)
+        => count == 0 ? "Go to the store and buy some more" : $"Take {it(count)} down and pass it around";
+
+    private static string it(int count)
+        => count == 1 ? "it" : "one";
+
+    private static string capitalize(string str)
+        => string.Concat(str[..1].ToUpperInvariant(), str[1..]);
 }
