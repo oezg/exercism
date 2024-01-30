a419e993b4c08d6a87f5f089b1c0b28e786c7c13
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-01-30 13:58:47 +0100

    type hint crystal

--- crystal/crystal-hunter/src/crystal_hunter.cr
+++ crystal/crystal-hunter/src/crystal_hunter.cr
@@ -1,17 +1,17 @@
 class Rules
-  def bonus_points?(power_up_active, touching_bandit)
+  def bonus_points?(power_up_active : Bool, touching_bandit : Bool) : Bool
     power_up_active && touching_bandit
   end
 
-  def score?(touching_power_up, touching_crystal)
+  def score?(touching_power_up : Bool, touching_crystal : Bool) : Bool
     touching_power_up || touching_crystal
   end
 
-  def lose?(power_up_active, touching_bandit)
+  def lose?(power_up_active : Bool, touching_bandit : Bool) : Bool
     !power_up_active && touching_bandit
   end
 
-  def win?(has_picked_up_all_crystals, power_up_active, touching_bandit)
+  def win?(has_picked_up_all_crystals : Bool, power_up_active : Bool, touching_bandit : Bool) : Bool
     has_picked_up_all_crystals && !lose?(power_up_active, touching_bandit)
   end
 end
