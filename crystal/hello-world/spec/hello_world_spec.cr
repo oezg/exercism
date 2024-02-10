6f09432ad98695122af74e97fa6e6e2469cca7da
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-02-10 12:33:27 +0100

    delete swift kotlin crystal

--- crystal/hello-world/spec/hello_world_spec.cr
+++
@@ -1,8 +0,0 @@
-require "spec"
-require "../src/*"
-
-describe "HelloWorld" do
-  it "Say Hello, World!" do
-    HelloWorld.new.hello.should eq("Hello, World!")
-  end
-end
--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,19 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
-  end
+  def today([]), do: nil
+  def today([today | _]), do: today
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
-  end
+  def increment_day_count([]), do: [1]
+  def increment_day_count([today | rest]), do: [today + 1 | rest]
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
-  end
 
-  def total(list) do
-    # Please implement the total/1 function
-  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | _]), do: true
+  def has_day_without_birds?([_ | tail]), do: has_day_without_birds?(tail)
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-  end
+  def total([]), do: 0
+  def total([head | tail]), do: head + total(tail)
+
+  def busy_days([]), do: 0
+  def busy_days([head | tail]) when head < 5, do: busy_days(tail)
+  def busy_days([_ | tail]), do: 1 + busy_days(tail)
 end
