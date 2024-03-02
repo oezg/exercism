63ca34d6d676c3d6f5b2037b848290a1e9240e1f
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-02 16:26:08 +0100

    js array map

---
+++ javascript/ozans-playlist/ozans-playlist.js
@@ -0,0 +1,54 @@
+/**
+ * Removes duplicate tracks from a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} new playlist with unique entries
+ */
+export function removeDuplicates(playlist) {
+  return [...new Set(playlist)];
+}
+
+/**
+ * Checks whether a playlist includes a track.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {boolean} whether the track is in the playlist
+ */
+export function hasTrack(playlist, track) {
+  return new Set(playlist).has(track);
+}
+
+/**
+ * Adds a track to a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function addTrack(playlist, track) {
+  return [...new Set(playlist).add(track)]
+}
+
+/**
+ * Deletes a track from a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function deleteTrack(playlist, track) {
+  const newPlaylist = new Set(playlist)
+  newPlaylist.delete(track)
+  return [...newPlaylist]
+}
+
+/**
+ * Lists the unique artists in a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} list of artists
+ */
+export function listArtists(playlist) {
+  return [...new Set(playlist.map(track => track.split(' - ')[1]))];
+}
