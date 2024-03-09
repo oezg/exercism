a62b9086f5766dd05eff960b01bc927af566d7ec
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-09 16:07:16 +0100

    first clojure exercises

---
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -0,0 +1,24 @@
+(ns cars-assemble)
+
+(def cars-per-hour 221)
+
+(defn success-rate [speed]
+  (cond (> speed 9) 0.77
+        (> speed 8) 0.8
+        (> speed 4) 0.9
+        (> speed 0) 1.0
+        :else 0.0)
+  )
+
+(defn production-rate
+  "Returns the assembly line's production rate per hour,
+   taking into account its success rate"
+  [speed]
+  (* cars-per-hour speed (success-rate speed))
+  )
+
+(defn working-items
+  "Calculates how many working cars are produced per minute"
+  [speed]
+  (int (/ (production-rate speed) 60))
+  )
