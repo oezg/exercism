0966c964498468034a4a4f97dd490b1793644557
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-13 21:55:57 +0100

    goon

--- fsharp/valentines-day/obj/Debug/net7.0/ValentinesDay.AssemblyInfo.fs
+++ fsharp/valentines-day/obj/Debug/net7.0/ValentinesDay.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("ValentinesDay")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+569450c305ab0acf7dfc565916f75abc2d9a305b")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77cae292af863b40657ad0f80aa03e68dae64677")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("ValentinesDay")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("ValentinesDay")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
