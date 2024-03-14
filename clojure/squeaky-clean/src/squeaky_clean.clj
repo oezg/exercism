3334b43bc485329875dfec3089484d2239712749
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-14 17:20:07 +0100

    start clj lists

--- clojure/squeaky-clean/src/squeaky_clean.clj
+++ clojure/squeaky-clean/src/squeaky_clean.clj
@@ -1,25 +1,12 @@
 (ns squeaky-clean
   (:require [clojure.string :as str]))
 
-(defn replaceControl 
-  [s]
-  (str/replace s #"[\u0000-\u001F\u007F-\u009F]" "CTRL")
-  )
-
-(defn replaceSpace
-  [s]
-  (str/replace (str/trim s) " " "_"))
-
-(defn kebapCase
-  [s]
-  
-  )
-
-
 (defn clean
   "Returns a clean Identifier"
   [s]
-  (replaceControl (replaceSpace s))
-  )
-
-
+  (-> s
+      (str/replace #"\s" "_")
+      (str/replace #"\p{IsControl}" "CTRL")
+      (str/replace #"-\p{Ll}" #(str/upper-case (get %1 1)))
+      (str/replace #"[^_\p{L}]" "")
+      (str/replace #"[\p{IsGreek}&&\p{IsLowerCase}]" "")))
