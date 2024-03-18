18062be87a54f572e44c41276b59f4717990a7ff
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-18 14:51:14 +0100

    rm extended

--- remove_directories.py
+++ remove_directories.py
@@ -2,10 +2,40 @@ import os
 import shutil
 
 
-def remove_directories(root_dir, directories):
+directories = (
+    "bin", 
+    "obj", 
+    ".vs", 
+    ".mono", 
+    "elm-stuff", 
+    "target", 
+    ".cpcache", 
+    "__pycache__",
+    ".gradle",
+    "build"
+)
+
+def test_directory(dirpath, dirname):
+    if "." == dirpath:
+        return dirname in (".mono", ".vscode", ".vs")
+    match dirpath.split("/")[1]:
+        case "elm":
+            return "elm-stuff" == dirname
+        case "csharp" | "fsharp":
+            return dirname in ("bin", "obj", ".vs")
+        case "clojure":
+            return dirname in ("target", ".cpcache")
+        case "java":
+            return dirname in ("bin", ".gradle", "build")
+        case "python":
+            return dirname in (".pytest_cache", "__pycache__")
+    return dirname == ".idea"
+
+
+def remove_directories(root_dir):
     for dirpath, dirnames, _ in os.walk(root_dir):
         for dirname in dirnames:
-            if dirname in directories:
+            if test_directory(dirpath, dirname):
                 direcotry_path = os.path.join(dirpath, dirname)
                 print(f"Removing directory: {direcotry_path}")
                 shutil.rmtree(direcotry_path)
@@ -28,7 +58,6 @@ def remove_files(root_dir):
                 os.remove(file_path)
     
 
-directories = ("bin", "obj", ".vs", ".mono", "elm-stuff", "target", ".cpcache", "__pycache__")
 root_directory = "."
-remove_directories(root_directory, directories)
+remove_directories(root_directory)
 remove_files(root_directory)
