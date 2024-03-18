e8d66de9e2d28bc55bb9e0fdaff2af58cbaffecd
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-18 12:26:47 +0100

    remove elmstuff

--- remove_directories.py
+++ remove_directories.py
@@ -11,6 +11,6 @@ def remove_directories(root_dir, directories):
                 shutil.rmtree(direcotry_path)
 
 
-directories = ("bin", "obj", ".vs", ".mono")
+directories = ("bin", "obj", ".vs", ".mono", "elm-stuff")
 root_directory = "."
 remove_directories(root_directory, directories)
