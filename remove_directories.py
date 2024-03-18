5805707a62e3ef41df53aaa3d8f213fe944696c1
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-18 13:08:49 +0100

    rm c# sln and py cache

--- remove_directories.py
+++ remove_directories.py
@@ -2,21 +2,33 @@ import os
 import shutil
 
 
-def remove_directories(root_dir, directories, files=None):
-    for dirpath, dirnames, filenames in os.walk(root_dir):
+def remove_directories(root_dir, directories):
+    for dirpath, dirnames, _ in os.walk(root_dir):
         for dirname in dirnames:
             if dirname in directories:
                 direcotry_path = os.path.join(dirpath, dirname)
                 print(f"Removing directory: {direcotry_path}")
                 shutil.rmtree(direcotry_path)
+
+
+def test_filename(dirpath, filename):
+    if "clojure" in dirpath:
+        return filename == ".lein-failures"
+    if "csharp" in dirpath:
+        return filename == ".editorconfig" or filename.endswith(".sln")
+    return False
+
+
+def remove_files(root_dir):
+    for dirpath, _, filenames in os.walk(root_dir):
         for filename in filenames:
-            if files is not None and filename in files:
+            if test_filename(dirpath, filename):
                 file_path = os.path.join(dirpath, filename)
                 print(f"Deleting file: {file_path}")
                 os.remove(file_path)
+    
 
-
-directories = ("bin", "obj", ".vs", ".mono", "elm-stuff", "target", ".cpcache")
-files = (".lein-failures", ".editorconfig")
+directories = ("bin", "obj", ".vs", ".mono", "elm-stuff", "target", ".cpcache", "__pycache__")
 root_directory = "."
-remove_directories(root_directory, directories, files)
+remove_directories(root_directory, directories)
+remove_files(root_directory)
