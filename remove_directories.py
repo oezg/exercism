be9298f40be3b1d124d850485e132699eb06e149
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-18 12:02:56 +0100

    remove vs and mono

---
+++ remove_directories.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+
+def remove_directories(root_dir, directories):
+    for dirpath, dirnames, _ in os.walk(root_dir):
+        for dirname in dirnames:
+            if dirname in directories:
+                direcotry_path = os.path.join(dirpath, dirname)
+                print(f"Removing {direcotry_path}")
+                shutil.rmtree(direcotry_path)
+
+
+directories = ("bin", "obj", ".vs", ".mono")
+root_directory = "."
+remove_directories(root_directory, directories)
