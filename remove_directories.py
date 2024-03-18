966cfa36b9b2565da6ec16692963e7e77a3a8bbd
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-18 12:42:47 +0100

    rm c# editorconfig and clj leinfailtures

--- remove_directories.py
+++ remove_directories.py
@@ -2,15 +2,21 @@ import os
 import shutil
 
 
-def remove_directories(root_dir, directories):
-    for dirpath, dirnames, _ in os.walk(root_dir):
+def remove_directories(root_dir, directories, files=None):
+    for dirpath, dirnames, filenames in os.walk(root_dir):
         for dirname in dirnames:
             if dirname in directories:
                 direcotry_path = os.path.join(dirpath, dirname)
-                print(f"Removing {direcotry_path}")
+                print(f"Removing directory: {direcotry_path}")
                 shutil.rmtree(direcotry_path)
+        for filename in filenames:
+            if files is not None and filename in files:
+                file_path = os.path.join(dirpath, filename)
+                print(f"Deleting file: {file_path}")
+                os.remove(file_path)
 
 
 directories = ("bin", "obj", ".vs", ".mono", "elm-stuff", "target", ".cpcache")
+files = (".lein-failures", ".editorconfig")
 root_directory = "."
-remove_directories(root_directory, directories)
+remove_directories(root_directory, directories, files)
