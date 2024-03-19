9f44b7ae03a0b757ba4c2fb8ae2627f336ab9d58
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-19 16:04:02 +0100

    java bits

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -4,9 +4,9 @@ using System.Linq;
 public static class Proverb
 {
     public static string[] Recite(string[] subjects) 
-        => subjects.Length == 0 ? Array.Empty<string>()
-            : subjects.Zip(subjects.Skip(1))
-            .Select(pair => $"For want of a {pair.First} the {pair.Second} was lost.")
-            .Append($"And all for the want of a {subjects[0]}.")
-            .ToArray();
+    => subjects.Length == 0 ? subjects
+        : subjects.Zip(subjects.Skip(1))
+        .Select(subjectPair => $"For want of a {subjectPair.First} the {subjectPair.Second} was lost.")
+        .Append($"And all for the want of a {subjects.First()}.")
+        .ToArray();
 }
