ae26ca340aaf9963d3d9138d8479ffb227e737a3
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-20 21:45:26 +0100

    elm tuples

---
+++ elm/go/src/Go.elm
@@ -0,0 +1,8 @@
+module Go exposing (..)
+
+import GoSupport exposing (..)
+
+
+applyRules : Game -> Rule -> NonValidatingRule -> Rule -> Rule -> Game
+applyRules game oneStonePerPointRule captureRule libertyRule koRule =
+    Debug.todo "Please implement the `applyRules` function"
---
+++ elm/tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
@@ -0,0 +1,56 @@
+module TisburyTreasureHunt exposing (..)
+
+
+type alias TreasureLocation =
+    ( Int, Char )
+
+
+type alias PlaceLocation =
+    ( Char, Int )
+
+
+type alias Treasure =
+    ( String, TreasureLocation )
+
+
+type alias Place =
+    ( String, PlaceLocation )
+
+
+placeLocationToTreasureLocation : PlaceLocation -> TreasureLocation
+placeLocationToTreasureLocation ( char, num ) =
+    ( num, char )
+
+
+treasureLocationMatchesPlaceLocation : PlaceLocation -> TreasureLocation -> Bool
+treasureLocationMatchesPlaceLocation placeLocation =
+    (==) (placeLocationToTreasureLocation placeLocation)
+
+
+countPlaceTreasures : Place -> List Treasure -> Int
+countPlaceTreasures ( _, placeLocation ) =
+    List.map Tuple.second
+        >> List.filter (treasureLocationMatchesPlaceLocation placeLocation)
+        >> List.length
+
+
+specialCaseSwapPossible : Treasure -> Place -> Treasure -> Bool
+specialCaseSwapPossible ( foundTreasure, _ ) ( place, _ ) ( desiredTreasure, _ ) =
+    case ( foundTreasure, place, desiredTreasure ) of
+        ( "Brass Spyglass", "Abandoned Lighthouse", _ ) ->
+            True
+
+        ( "Amethyst Octopus", "Stormy Breakwater", "Crystal Crab" ) ->
+            True
+
+        ( "Amethyst Octopus", "Stormy Breakwater", "Glass Starfish" ) ->
+            True
+
+        ( "Vintage Pirate Hat", "Harbor Managers Office", "Model Ship in Large Bottle" ) ->
+            True
+
+        ( "Vintage Pirate Hat", "Harbor Managers Office", "Antique Glass Fishnet Float" ) ->
+            True
+
+        _ ->
+            False
