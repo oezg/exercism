55ab935a25e84b4c82d5fd99ef77a4e8a8dcf8da
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-21 08:31:11 +0100

    clj cards

--- clojure/card-games/src/card_games.clj
+++ clojure/card-games/src/card_games.clj
@@ -33,33 +33,25 @@
    (== (card-average hand) (nth hand (/ (count hand) 2)))
    (== (card-average hand) (/ (+ (first hand) (last hand)) 2))))
 
-(defn index-elements
-  "This function takes a list lst as input, maps each element of the
-     list along with its index using map-indexed, then filters out the
-     elements at chosen indexes using filter, and finally extracts the
-     elements from the filtered pairs using second."
-  [lst f]
-  (map second (filter #(f (first %)) (map-indexed vector lst))))
-
-
 (defn average-even-odd?
   "Returns true if the average of the cards at even indexes
    is the same as the average of the cards at odd indexes."
   [hand]
-  (= (card-average (index-elements hand odd?))
-     (card-average (index-elements hand even?))))
+  (let [m (fn [x]
+            (card-average (map second (filter #(x (first %)) (map-indexed vector hand)))))]
+    (= (m odd?) (m even?)))
 
-(defn double-last
-  "This function takes a list lst as input. It first removes the last
+  (defn double-last
+    "This function takes a list lst as input. It first removes the last
    element of the list using butlast, then doubles the last element
    using (* 2 (last lst)), and finally adds the doubled last element
    to the modified list using conj."
-  [lst]
-  (conj (vec (butlast lst)) (* 2 (last lst))))
+    [lst]
+    (conj (vec (butlast lst)) (* 2 (last lst))))
 
 
-(defn maybe-double-last
-  "If the last card is a Jack (11), doubles its value
+  (defn maybe-double-last
+    "If the last card is a Jack (11), doubles its value
    before returning the hand."
-  [hand]
-  (if (not= 11 (last hand)) hand (double-last hand)))
+    [hand]
+    (if (not= 11 (last hand)) hand (double-last hand)))
