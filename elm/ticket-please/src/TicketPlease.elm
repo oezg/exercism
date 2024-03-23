3ca32bf9ea2904ebb1ebb32493c6c1faf3113951
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-23 15:23:34 +0100

    elm dict and patternmatching

---
+++ elm/ticket-please/src/TicketPlease.elm
@@ -0,0 +1,47 @@
+module TicketPlease exposing (..)
+
+import List exposing (member)
+import TicketPleaseSupport exposing (Status(..), Ticket(..), User(..))
+
+
+devteam : List String
+devteam =
+    [ "Alice", "Bob", "Charlie" ]
+
+
+emptyComment : ( User, String ) -> Bool
+emptyComment ( _, comment ) =
+    String.isEmpty comment
+
+
+numberOfCreatorComments : Ticket -> Int
+numberOfCreatorComments (Ticket { createdBy, comments }) =
+    let
+        ( creator, _ ) =
+            createdBy
+    in
+    List.length <|
+        List.filter (\( user, _ ) -> user == creator) comments
+
+
+assignedToDevTeam : Ticket -> Bool
+assignedToDevTeam (Ticket { assignedTo }) =
+    case assignedTo of
+        Just (User user) ->
+            member user devteam
+
+        _ ->
+            False
+
+
+assignTicketTo : User -> Ticket -> Ticket
+assignTicketTo user (Ticket ({ status } as original)) =
+    case status of
+        Archived ->
+            Ticket original
+
+        New ->
+            Ticket { original | status = InProgress, assignedTo = Just user }
+
+        _ ->
+            Ticket { original | assignedTo = Just user }
