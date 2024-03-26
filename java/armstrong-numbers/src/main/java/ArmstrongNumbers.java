8ebad2f4a87bdc3e1ad5cbb885eb2ba2cfc443d4
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-26 11:49:13 +0100

    java refactor armstrong recursive

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,17 +1,25 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntUnaryOperator;
 
 class ArmstrongNumbers {
 
-  IntStream digits(String number) {
-    return Arrays.stream(number.split("")).mapToInt(Integer::parseInt);
+  private List<Integer> digits(List<Integer> acc, int number) {
+    if (number == 0) {
+      return acc;
+    }
+    var x = number % 10;
+    acc.add(x);
+    return digits(acc, number / 10);
   }
 
-  int armstrong(String number) {
-    return digits(number).map(n -> (int) Math.pow(n, number.length())).sum();
+  private int armstrong(int number) {
+    var lst = digits(new ArrayList<Integer>(), number);
+    IntUnaryOperator power = n -> (int) Math.pow(n, lst.size());
+    return lst.stream().mapToInt(Integer::intValue).map(power).sum();
   }
 
   boolean isArmstrongNumber(int numberToCheck) {
-    return numberToCheck == armstrong(Integer.toString(numberToCheck));
+    return numberToCheck == armstrong(numberToCheck);
   }
 }
