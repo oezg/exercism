51755e669f0dc4c1956c32c916b79626e42b3b22
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-26 19:14:34 +0100

    clj acronym refactor

--- clojure/acronym/src/acronym.clj
+++ clojure/acronym/src/acronym.clj
@@ -1,7 +1,19 @@
 (ns acronym
   (:require [clojure.string :as str]))
 
+(defn insert-space-before-uppercase [s]
+  (str/replace s #"(?<=[a-z])(?=[A-Z])" " "))
+
+(defn split-space-and-hyphen [s]
+  (str/split s #"[ -]"))
+
 (defn acronym
   "Converts phrase to its acronym."
   [phrase]
-  (str/join (map str/upper-case (map str (map first (str/split phrase #"[ -]"))))))
+  (->> phrase
+       (insert-space-before-uppercase)
+       (split-space-and-hyphen)
+       (map first)
+       (map str)
+       (map str/upper-case)
+       (str/join)))
