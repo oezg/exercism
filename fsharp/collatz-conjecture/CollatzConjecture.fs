55a2bdefa3736428e1f97cf4de90f2053d02219a
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-03-28 08:53:44 +0100

    f# collatz

--- fsharp/collatz-conjecture/CollatzConjecture.fs
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -1,17 +1,9 @@
 module CollatzConjecture
 
-let (|Even|Odd|) number = if number % 2 = 0 then Even else Odd
-
-let (|InRange|_|) n = if n > 0 then Some() else None 
-
-let steps (number: int): int option = 
-    
-    let rec collatz acc num = 
-        match num with
-        | 1 -> acc
-        | Even -> collatz (acc + 1) (num / 2)
-        | Odd -> collatz (acc + 1) (3 * num + 1)
-    
-    match number with
-    | InRange -> collatz 0 number |> Some
-    | _ -> None
+let steps =
+    Seq.unfold (function
+        | outOfRange when outOfRange < 1 -> None
+        | 1 -> Some(1, 0)
+        | even when even % 2 = 0 -> Some(even, even / 2)
+        | odd -> Some(odd, 3 * odd + 1))
+    >> Seq.tryFindIndex ((=) 1)
