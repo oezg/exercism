8f37a64c6fc3783073064ce2a789a2c346750257
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-05 07:58:54 +0200

    elm random

--- elm/maze-maker/src/MazeMaker.elm
+++ elm/maze-maker/src/MazeMaker.elm
@@ -1,6 +1,6 @@
 module MazeMaker exposing (..)
 
-import Random exposing (Generator, generate)
+import Random exposing (Generator)
 
 
 type Maze
@@ -17,29 +17,40 @@ type Treasure
 
 deadend : Generator Maze
 deadend =
-    Random.lazy (\_ -> Random.constant DeadEnd)
+    Random.constant DeadEnd
 
 
 treasure : Generator Treasure
 treasure =
-    Random.lazy (\_ -> Random.uniform Gold [ Diamond, Friendship ])
+    Random.uniform Gold [ Diamond, Friendship ]
 
 
 room : Generator Maze
 room =
-    Random.lazy (\_ -> Random.map (\t -> Room t) treasure)
+    Random.map (\t -> Room t) treasure
 
 
 branch : Generator Maze -> Generator Maze
 branch mazeGenerator =
-    Random.lazy (\_ -> mazeGenerator)
+    Random.int 2 4
+        |> Random.andThen (\n -> Random.list n mazeGenerator |> Random.map Branch)
 
 
 maze : Generator Maze
 maze =
-    Random.lazy (\_ -> Random.weighted ( 60, deadend ) [ ( 15, room ), ( 25, branch maze ) ])
+    Random.weighted
+        ( 12, deadend )
+        [ ( 3, room )
+        , ( 5, (\_ -> maze) |> Random.lazy |> branch )
+        ]
+        |> Random.andThen identity
 
 
 mazeOfDepth : Int -> Generator Maze
 mazeOfDepth depth =
-    Random.lazy (\_ -> Debug.todo "Please implement mazeOfDepth")
+    if depth == 0 then
+        Random.uniform deadend [ room ] |> Random.andThen identity
+
+    else
+        mazeOfDepth (depth - 1)
+            |> branch
