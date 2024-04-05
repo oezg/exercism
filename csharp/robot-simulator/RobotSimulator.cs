47ea214622f1cea03fd66de7744e7e2394b1c5ff
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-05 14:21:03 +0200

    refactor robot simulator enum extensions

--- csharp/robot-simulator/RobotSimulator.cs
+++ csharp/robot-simulator/RobotSimulator.cs
@@ -1,4 +1,28 @@
+using System.Linq;
 using System;
+
+public class RobotSimulator
+{
+    public void Move(string instructions)
+    {
+        foreach (char instruction in instructions)
+            switch (instruction)
+            {
+                case 'A': (X, Y) = (X + Direction.X(), Y + Direction.Y()); break;
+                case 'R': Direction = Direction.Right(); break;
+                case 'L': Direction = Direction.Left(); break;
+                default: throw new ArgumentOutOfRangeException(nameof(instructions), "invalid");
+            }
+    }
+
+    public RobotSimulator(Direction direction, int x, int y) => (Direction, X, Y) = (direction, x, y);
+
+    public Direction Direction { get; set; }
+
+    public int X { get; set; }
+
+    public int Y { get; set; }
+}
 
 public enum Direction
 {
@@ -8,87 +32,33 @@ public enum Direction
     West
 }
 
-public class RobotSimulator
+public static class DirectionExtensions
 {
-    #region constructors
-    public RobotSimulator(Direction direction, int x, int y)
-    {
-        Direction = direction;
-        X = x;
-        Y = y;
-    }
-    #endregion
-
-    #region properties
-    public Direction Direction { get; set; }
-
-    public int X { get; set; }
-
-    public int Y { get; set; }
-    #endregion
-
-    #region methods
-    public void Move(string instructions)
-    {
-        foreach (char instruction in instructions)
+    public static int X(this Direction direction) => toTuple(direction).X;
+    public static int Y(this Direction direction) => toTuple(direction).Y;
+    public static Direction Right(this Direction direction) => fromTuple(right(toTuple(direction)));
+    public static Direction Left(this Direction direction) => fromTuple(left(toTuple(direction)));
+    static (int X, int Y) toTuple(Direction direction) =>
+        direction switch
         {
-            switch (instruction)
-            {
-                case 'A':
-                    switch (Direction)
-                    {
-                        case Direction.North:
-                            Y += 1;
-                            break;
-                        case Direction.East:
-                            X += 1;
-                            break;
-                        case Direction.West:
-                            X -= 1;
-                            break;
-                        case Direction.South:
-                            Y -= 1; 
-                            break;
-                    }
-                    break;
-                case 'R':
-                    switch (Direction)
-                    {
-                        case Direction.North:
-                            Direction = Direction.East;
-                            break;
-                        case Direction.East:
-                            Direction = Direction.South;
-                            break;
-                        case Direction.West:
-                            Direction = Direction.North;
-                            break;
-                        case Direction.South:
-                            Direction = Direction.West;
-                            break;
-                    }
-                    break;
-                case 'L':
-                    switch (Direction)
-                    {
-                        case Direction.North:
-                            Direction = Direction.West;
-                            break;
-                        case Direction.East:
-                            Direction = Direction.North;
-                            break;
-                        case Direction.West:
-                            Direction = Direction.South;
-                            break;
-                        case Direction.South:
-                            Direction = Direction.East;
-                            break;
-                    }
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(instruction + " is not one of 'A', 'L' or 'R'!");
-            }
-        }
-    }
-    #endregion
+            Direction.North => (0, 1),
+            Direction.East => (1, 0),
+            Direction.South => (0, -1),
+            Direction.West => (-1, 0),
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), "invalid")
+        };
+
+    static Direction fromTuple((int x, int y) direction) =>
+        direction switch
+        {
+            (0, 1) => Direction.North,
+            (1, 0) => Direction.East,
+            (0, -1) => Direction.South,
+            (-1, 0) => Direction.West,
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), "invalid")
+        };
+
+    static (int x, int y) right((int X, int Y) direction) => (direction.Y, -direction.X);
+
+    static (int x, int y) left((int X, int Y) direction) => (-direction.Y, direction.X);
 }
