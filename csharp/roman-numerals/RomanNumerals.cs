9c69dab6947e7a314e6732efdbb7e39bb07d0406
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-05 17:16:27 +0200

    c# roman refactor

--- csharp/roman-numerals/RomanNumerals.cs
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -1,71 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 public static class RomanNumeralExtension
 {
-    private static readonly Dictionary<int, Dictionary<char, string>> _romans;
+    public static string ToRoman(this int value) =>
+        string.Join("", digits(value).Select((digit, index) => romans[index][digit]).Reverse());
 
-    static RomanNumeralExtension()
-        => _romans = new Dictionary<int, Dictionary<char, string>>()
-        {
-            [0] = new Dictionary<char, string>
-            {
-                ['0'] = "",
-                ['1'] = "M",
-                ['2'] = "MM",
-                ['3'] = "MMM",
-            },
-            [1] = new Dictionary<char, string>
-            {
-                ['0'] = "",
-                ['1'] = "C",
-                ['2'] = "CC",
-                ['3'] = "CCC",
-                ['4'] = "CD",
-                ['5'] = "D",
-                ['6'] = "DC",
-                ['7'] = "DCC",
-                ['8'] = "DCC",
-                ['9'] = "CM",
-            }, 
-            [2] = new Dictionary<char, string> 
-            {
-                ['0'] = "",
-                ['1'] = "X",
-                ['2'] = "XX",
-                ['3'] = "XXX",
-                ['4'] = "XL",
-                ['5'] = "L",
-                ['6'] = "LX",
-                ['7'] = "LXX",
-                ['8'] = "LXXX",
-                ['9'] = "XC",
-            },
-            [3] = new Dictionary<char, string> 
-            {
-                ['0'] = "",
-                ['1'] = "I",
-                ['2'] = "II",
-                ['3'] = "III",
-                ['4'] = "IV",
-                ['5'] = "V",
-                ['6'] = "VI",
-                ['7'] = "VII",
-                ['8'] = "VIII",
-                ['9'] = "IX",
-            },
-        };
+    static readonly Dictionary<int, string[]> romans = new()
+    {
+        [0] = new string[] { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" },
+        [1] = new string[] { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" },
+        [2] = new string[] { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" },
+        [3] = new string[] { "", "M", "MM", "MMM", "", "", "", "", "", "", "" }
+    };
 
-    public static string ToRoman(this int value)
-        => value
-        is > 0 and < 4000 
-        ? value
-        .ToString()
-        .PadLeft(4, '0')
-        .Select((digit, index) => _romans[index][digit])
-        .Aggregate(new StringBuilder(), (sb, roman) => sb.Append(roman))
-        .ToString()
-        : throw new ArgumentOutOfRangeException($"{nameof(value)} must be in range (0, 4000)");
+    static IEnumerable<int> digits(int value)
+    {
+        for (int n = value; n > 0; n /= 10) yield return n % 10;
+    }
 }
