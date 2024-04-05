c08ae80ecfc11deafe3fa70c668418f4a0cb3de5
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-05 11:16:14 +0200

    refactor queen attack

--- csharp/queen-attack/QueenAttack.cs
+++ csharp/queen-attack/QueenAttack.cs
@@ -1,26 +1,19 @@
 using System;
 
-public class Queen
-{
-    public Queen(int row, int column)
-    {
-        Row = row;
-        Column = column;
-    }
-
-    public int Row { get; }
-    public int Column { get; }
-}
-
 public static class QueenAttack
 {
-    public static bool CanAttack(Queen white, Queen black)
-        => white != null && black != null 
-        && (white.Row == black.Row || white.Column == black.Column 
-        || Math.Abs(white.Row - black.Row) == Math.Abs(white.Column - black.Column));
+    public static bool CanAttack((int Row, int Column) white, (int Row, int Column) black) =>
+        Math.Abs(white.Row - black.Row) == Math.Abs(white.Column - black.Column)
+        || white.Row == black.Row
+        || white.Column == black.Column;
 
-    public static Queen Create(int row, int column) 
-        => row < 0 || column < 0 || row > 7 || column > 7
-            ? throw new ArgumentOutOfRangeException("row and column cannot be less than 0 or more than 7")
-            : new Queen(row, column);
+    public static (int Row, int Column) Create(int row, int column) =>
+        (row, column) switch
+        {
+            var (r, _) when r < 0 || r > 7 =>
+                throw new ArgumentOutOfRangeException(nameof(row), "must be between 0 and 7"),
+            var (_, c) when c < 0 || c > 7 =>
+                throw new ArgumentOutOfRangeException(nameof(column), "must be between 0 and 7"),
+            _ => (row, column)
+        };
 }
