b72c654e30312c9b21260c86934f82f465653a19
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-12 15:04:04 +0200

    refactor lsp

--- go/largest-series-product/largest_series_product.go
+++ go/largest-series-product/largest_series_product.go
@@ -1,74 +1,56 @@
+// Package lsproduct is utility to look for patterns in the long sequence of digits in the encrypted signal.
 package lsproduct
 
 import (
 	"errors"
 )
 
-var (
-	ErrInvalidInput error = errors.New("digits input must only contain digits")
-	ErrNegativeSpan error = errors.New("span must not be negative")
-	ErrTooLargeSpan error = errors.New("span must not be larger than input length")
-)
-
+// LargestSeriesProduct finds the largest product among series of given span in the input.
 func LargestSeriesProduct(input string, span int) (int64, error) {
-	if span < 0 {
-		return 0, ErrNegativeSpan
+	if span < 0 || span > len(input) {
+		return 0, errors.New("span must be between 0 and input length")
 	}
-	if span > len(input) {
-		return 0, ErrTooLargeSpan
-	}
-	digits, err := toDigits(input)
-	if err != nil {
-		return 0, err
-	}
-	series := toSeries(digits, span)
-	products := toProducts(series)
-	return largestProduct(products), nil
-}
-
-func toDigits(input string) ([]int64, error) {
-	out := []int64{}
-	for _, digit := range input {
+	largestProduct := int64(0)
+	buffer := make([]int64, span)
+	for i, digit := range input {
 		if digit < '0' || digit > '9' {
-			return nil, ErrInvalidInput
+			return 0, errors.New("digits input must only contain digits")
 		}
-		out = append(out, int64(digit-'0'))
-	}
-	return out, nil
-}
 
-func toSeries(digits []int64, span int) [][]int64 {
-	out := [][]int64{}
-	for i := 0; i < len(digits)-span+1; i++ {
-		out = append(out, digits[i:i+span])
-	}
-	return out
-}
+		buffer[i%span] = int64(digit - '0')
 
-func toProducts(series [][]int64) []int64 {
-	out := make([]int64, 0, len(series))
-	for _, serie := range series {
+		if i < span-1 {
+			continue
+		}
+
 		product := int64(1)
-		for _, digit := range serie {
-			product *= digit
+		for _, v := range buffer {
+			product *= v
 		}
-		out = append(out, product)
+
+		if largestProduct < product {
+			largestProduct = product
+		}
 	}
-	return out
+	return largestProduct, nil
 }
 
-func largestProduct(products []int64) int64 {
-	largest := int64(0)
-	for _, product := range products {
-		if largest < product {
-			largest = product
-		}
-	}
-	return largest
-}
+// 7 Buffer (smaller memory footprint)
+// BenchmarkLargestSeriesProduct-4           561106              1847 ns/op             312 B/op         14 allocs/op
+
+// 6 One pass
+// BenchmarkLargestSeriesProduct-4           869005              1171 ns/op            1024 B/op         14 allocs/op
+
+// 5 Two pass
+// BenchmarkLargestSeriesProduct-4           444876              2631 ns/op            2576 B/op         48 allocs/op
+
+// 4 Anonymous errors
+// BenchmarkLargestSeriesProduct-4           177628              6471 ns/op            8928 B/op         92 allocs/op
+
+// 3 Different errors
+// BenchmarkLargestSeriesProduct-4           182761              6332 ns/op            8864 B/op         88 allocs/op
 
 // goos: linux
 // goarch: amd64
 // pkg: lsproduct
 // cpu: Intel(R) Core(TM) i5-6500 CPU @ 3.20GHz
-// BenchmarkLargestSeriesProduct-4           182761              6332 ns/op            8864 B/op         88 allocs/op
