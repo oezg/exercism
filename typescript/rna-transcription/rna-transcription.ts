58b63bd3e6d085313e1558d427536c135a3ea521
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-12 15:17:21 +0200

    ts rna

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,8 +1,8 @@
 export function toRna(strand: string): string {
-  return strand.split('').map(x => abc[x]).join('');
+  return strand.split('').map(complement).join('');
 }
 
-const abc = new Map<string, string>([
+const rnaTranslationTable = new Map<string, string>([
   ['A', 'U'],
   ['C', 'G'],
   ['G', 'C'],
@@ -10,16 +10,8 @@ const abc = new Map<string, string>([
 ]);
 
 const complement = (base: string): string => {
-switch (base) {
-  case 'A':
-    return 'U';
-  case 'C':
-    return 'G';
-  case 'G':
-    return 'C';
-  case 'T':
-    return 'A';
-  default:
+  const translation = rnaTranslationTable.get(base);
+  if (translation === undefined)
     throw new Error('Invalid input DNA.');
-}
+  return translation;
 }
