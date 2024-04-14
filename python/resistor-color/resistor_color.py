ac1de28045c1c48f0750ef6f085e5cd0dfd9846f
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-14 23:18:59 +0200

    py resistor

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,19 @@
+color_codes = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
+
+def color_code(color):
+    return color_codes[color]
+
+
+def colors():
+    return list(color_codes.keys())
