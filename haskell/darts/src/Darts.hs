3af33bb94b43bf5d17e6b2af8577ea49519dedd4
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-15 23:44:35 +0200

    hskl darts

--- haskell/darts/src/Darts.hs
+++ haskell/darts/src/Darts.hs
@@ -1,4 +1,11 @@
 module Darts (score) where
 
 score :: Float -> Float -> Int
-score x y = case sqrt (x * x + y * y) of {}
+score x y
+  | radius <= innerCircle = 10
+  | radius <= middleCircle = 5
+  | radius <= outerCircle = 1
+  | otherwise = 0
+  where
+    radius = sqrt (x * x + y * y)
+    (innerCircle, middleCircle, outerCircle) = (1, 5, 10)
