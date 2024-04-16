35cfe0b57a6496758a02a6d432971c0af724c334
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-16 10:03:51 +0200

    julia nuc count

--- julia/nucleotide-count/nucleotide-count.jl
+++ julia/nucleotide-count/nucleotide-count.jl
@@ -7,5 +7,13 @@ Invalid strands raise a `DomainError`.
 
 """
 function count_nucleotides(strand)
-
+    result = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0)
+    for n in strand
+        try
+            result[n] = result[n] + 1
+        catch KeyError
+            throw(DomainError(n))
+        end
+    end
+    return result
 end
