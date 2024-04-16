fd2d3466714b7fca0d23dda4fd3758f0a5ecb21c
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-16 08:22:56 +0200

    julia

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,14 +1,20 @@
 class Darts {
+    final int innerCircle = 1;
+    final int middleCircle = 5;
+    final int outerCircle = 10;
+
     int score(double xOfDart, double yOfDart) {
         double radius = Math.sqrt(xOfDart * xOfDart + yOfDart * yOfDart);
-        int result = 0;
-        if (radius <= 1) {
-            result = 10;
-        } else if (radius <= 5) {
-            result = 5;
-        } else if (radius <= 10) {
-            result = 1;
+
+        if (radius <= innerCircle) {
+            return 10;
         }
-        return result;
+        if (radius <= middleCircle) {
+            return 5;
+        }
+        if (radius <= outerCircle) {
+            return 1;
+        }
+        return 0;
     }
 }
