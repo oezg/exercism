9e6609ab6dbe39cbcddb530cdfbb23cdb03e4614
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-19 15:34:32 +0200

    elm dict

--- elm/grade-school/src/GradeSchool.elm
+++ elm/grade-school/src/GradeSchool.elm
@@ -1,7 +1,6 @@
 module GradeSchool exposing (Grade, Result(..), School, Student, addStudent, allStudents, emptySchool, studentsInGrade)
 
 import Dict exposing (..)
-import Set
 
 
 type alias Grade =
@@ -28,36 +27,11 @@ emptySchool =
 
 addStudent : Grade -> Student -> School -> ( Result, School )
 addStudent grade student school =
-    let
-        result =
-            if List.member student <| allStudents school then
-                Duplicate
+    if List.member student (allStudents school) then
+        ( Duplicate, school )
 
-            else
-                Added
-
-        update maybeListStudents =
-            case maybeListStudents of
-                Nothing ->
-                    List.singleton student
-                        |> Just
-
-                Just list ->
-                    list
-                        |> Set.fromList
-                        |> Set.insert student
-                        |> Set.toList
-                        |> Just
-
-        newSchool =
-            case result of
-                Duplicate ->
-                    school
-
-                Added ->
-                    Dict.update grade update school
-    in
-    ( result, newSchool )
+    else
+        ( Added, Dict.insert grade (List.sort (student :: studentsInGrade grade school)) school )
 
 
 studentsInGrade : Grade -> School -> List Student
