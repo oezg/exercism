e9e9b09b2b7b0d4dc716e37f0bdc84f9745f34e5
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-22 23:11:05 +0200

    err

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,26 +1,19 @@
 export class Matrix {
-  s: string
   rows_: number[][]
   columns_: number[][]
 
   constructor(s: string) {
-    this.s = s;
-    this.rows_= this.s.split('\n').map(line => line.split(' ').map(x => +x));
-    this.columns_ = new Array(this.rows.length).fill(0).map(x => new Array(this.rows.length).fill(0));
-    for (let index = 0; index < this.rows.length; index++) {
-      const row = this.rows[index];
-      for (let jindex = 0; jindex < row.length; jindex++) {
-        const element = row[jindex];
-        this.columns_[jindex][index] = element;
-      }
-    }
+    this.rows_= s.split('\n').map(line => line.split(' ').map(x => +x));
+    this.columns_ = this.rows_[0].map((_, colIndex) => this.rows_.map(row => row[colIndex]))
   }
 
   get rows(): number[][] {
-    return this.rows_;
+    return this.deepCopy(this.rows_);
   }
 
   get columns(): number[][] {
-    return JSON.parse(JSON.stringify(this.columns_));
+    return this.deepCopy(this.columns_);
   }
+
+  private readonly deepCopy = (matrix: number[][]): number[][] => JSON.parse(JSON.stringify(matrix));
 }
