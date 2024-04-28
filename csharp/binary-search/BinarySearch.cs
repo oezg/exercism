26f6051266d5cc6815aa707fb679175140a9c15f
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-29 00:42:34 +0200

    triangle

--- csharp/binary-search/BinarySearch.cs
+++ csharp/binary-search/BinarySearch.cs
@@ -5,21 +5,15 @@ public static class BinarySearch
     public static int Find(int[] input, int value)
     {
         int lower = 0, upper = input.Length, middle;
-        while(lower < upper)
+        while (lower < upper)
         {
             middle = (upper + lower) / 2;
             if (input[middle] == value)
-            {
                 return middle;
-            }
-            else if (input[middle] > value)
-            {
+            if (input[middle] > value)
                 upper = middle;
-            }
             else
-            {
                 lower = middle + 1;
-            }
         }
         return -1;
     }
--- fsharp/triangle/Triangle.fs
+++ fsharp/triangle/Triangle.fs
@@ -1,20 +1,13 @@
 module Triangle
 
-let isValid =
-    function
-    | [ 0.0; 0.0; 0.0 ] -> false
-    | [ a; b; c ] when a < 0.0 || b < 0.0 || c < 0.0 -> false
-    | [ a; b; c ] when a + b < c || b + c < a || a + c < b -> false
-    | [ _; _; _ ] -> true
+let private triangleWithDistinctSides sides triangle =
+    match triangle with
+    | [ a; b; c ] -> a > 0. && b > 0. && c > 0. && a + b >= c && b + c >= a && a + c >= b
     | _ -> false
+    && List.exists (fun n -> triangle |> List.distinct |> List.length = n) sides
 
-let distinctSides sides = List.distinct sides |> List.length
+let equilateral = triangleWithDistinctSides <| List.singleton 1
 
-let equilateral triangle =
-    isValid triangle && (distinctSides triangle = 1)
+let isosceles = triangleWithDistinctSides <| [ 1; 2 ]
 
-let isosceles triangle =
-    isValid triangle && distinctSides triangle < 3
-
-let scalene triangle =
-    isValid triangle && distinctSides triangle = 3
+let scalene = triangleWithDistinctSides <| List.singleton 3
