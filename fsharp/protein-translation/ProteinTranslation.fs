04c702e98b2c83ce097a91a990d1a83dd167ee7e
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-04-30 17:13:48 +0200

    type definition

--- fsharp/protein-translation/ProteinTranslation.fs
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -1,3 +1,55 @@
 module ProteinTranslation
 
+type AminoAcid =
+    | Methionine
+    | Phenylalanine
+    | Leucine
+    | Serine
+    | Tyrosine
+    | Cysteine
+    | Tryptophan
+
+type Codon =
+    | AUG
+    | UUU
+    | UUC
+    | UUA
+    | UUG
+    | UCU
+    | UCC
+    | UCA
+    | UCG
+    | UAU
+    | UAC
+    | UGU
+    | UGC
+    | UGG
+    | UAA
+    | UAG
+    | UGA
+
+
+let translate (codon: Codon) : AminoAcid option =
+    match codon with
+    | AUG -> Some Methionine
+    | UUU
+    | UUC -> Some Phenylalanine
+    | UUA
+    | UUG -> Some Leucine
+    | UCU
+    | UCC
+    | UCA
+    | UCG -> Some Serine
+    | UAU
+    | UAC -> Some Tyrosine
+    | UGU
+    | UGC -> Some Cysteine
+    | UGG -> Some Tryptophan
+    | UAA
+    | UAG
+    | UGA -> None
+
+
+
+
 let proteins rna = []
