5b038c6910f973bb25a00a8189b75fec263fcce4
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-05-13 23:53:42 +0200

    f# protein

--- fsharp/protein-translation/ProteinTranslation.fs
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -8,8 +8,6 @@ type private Nucleobase =
     | C
     | G
 
-type private Triplet = Triplet of Nucleobase * Nucleobase * Nucleobase
-
 type private AminoAcid =
     | Methionine
     | Phenylalanine
@@ -19,11 +17,15 @@ type private AminoAcid =
     | Cysteine
     | Tryptophan
 
-type private Protein = Protein of AminoAcid list
+type private RNA = RNA of Nucleobase list
+
+type private Triplet = Triplet of Nucleobase * Nucleobase * Nucleobase
 
 type private Codon =
     | Coding of AminoAcid
     | Stop
+
+type private Protein = Protein of AminoAcid list
 
 let private nucleobaseDictionary =
     [| 'A', A; 'U', U; 'C', C; 'G', G |] |> Map.ofArray
@@ -31,40 +33,49 @@ let private nucleobaseDictionary =
 let private toBase (letter: char) : Result<Nucleobase, string> =
     nucleobaseDictionary
     |> Map.tryFind letter
-    |> Result.requireSome $"invalid nucleobase: {letter}"
+    |> Result.requireSome $"Invalid nucleobase: {letter}"
 
 let private toTriplet: Nucleobase list -> Result<Triplet, string> =
     function
     | [ a; b; c ] -> Ok(Triplet(a, b, c))
-    | notTriplet -> Error $"invalid triplet length: {List.length notTriplet}"
+    | notTriplet -> Error $"Invalid triplet length: {List.length notTriplet}"
 
 let private toCodon (Triplet(a, b, c): Triplet) : Result<Codon, string> =
     match (a, b, c) with
-    | A, U, G -> Ok(Coding Methionine)
-    | U, U, (U | C) -> Ok(Coding Phenylalanine)
-    | U, U, (A | G) -> Ok(Coding Leucine)
-    | U, C, _ -> Ok(Coding Serine)
-    | U, A, (U | C) -> Ok(Coding Tyrosine)
-    | U, G, (U | C) -> Ok(Coding Cysteine)
-    | U, G, G -> Ok(Coding Tryptophan)
-    | U, A, (A | G) -> Ok(Stop)
-    | U, G, A -> Ok(Stop)
-    | notCodon -> Error $"invalid codon: {notCodon}"
-
-let private isCoding codon =
-    match codon with
-    | Stop -> false
-    | _ -> true
-
-let private toProtein (codons: Codon list) : Protein =
-    codons |> List.takeWhile isCoding |> List.map (fun (Coding aa) -> aa) |> Protein
+    | A, U, G -> Methionine |> Coding |> Ok
+    | U, U, (U | C) -> Phenylalanine |> Coding |> Ok
+    | U, U, (A | G) -> Leucine |> Coding |> Ok
+    | U, C, _ -> Serine |> Coding |> Ok
+    | U, A, (U | C) -> Tyrosine |> Coding |> Ok
+    | U, G, (U | C) -> Cysteine |> Coding |> Ok
+    | U, G, G -> Tryptophan |> Coding |> Ok
+    | U, A, (A | G) -> Stop |> Ok
+    | U, G, A -> Stop |> Ok
+    | notCodon -> Error $"Invalid codon: {notCodon}"
 
 let private resultExpressionProtein (strand: string) : Result<Protein, string> =
     result {
-        let! rna = strand |> List.ofSeq |> List.traverseResultM toBase
-        let! triplets = rna |> List.chunkBySize 3 |> List.traverseResultM toTriplet
-        let! codons = triplets |> List.traverseResultM toCodon
-        return toProtein codons
+        let! (rna: RNA) = strand |> List.ofSeq |> List.traverseResultM toBase |> Result.map RNA
+
+        let! (codons: Codon list) =
+            match rna with
+            | RNA bases ->
+                bases
+                |> List.chunkBySize 3
+                |> List.traverseResultM toTriplet
+                |> Result.bind (List.traverseResultM toCodon)
+
+        let (protein: Protein) =
+            codons
+            |> List.takeWhile (function
+                | Stop -> false
+                | _ -> true)
+            |> List.map (function
+                | Coding aminoAcid -> aminoAcid
+                | Stop -> failwith "Incomplete pattern match warning")
+            |> Protein
+
+        return protein
     }
 
 let proteins (rna: string) : string list =
---
+++ fsharp/protein-translation/parenstopipe.py
@@ -0,0 +1,13 @@
+import re
+
+pattern = re.compile(r"Ok\(Coding (\w+)\)")
+
+with open("text3.txt", "r+", encoding="utf-8") as f:
+    text = []
+    for line in f.readlines():
+        newline = pattern.sub(r"\1 |> Coding |> Ok", line)
+        # newline = line.replace("| ", "| [").replace(",", ";").replace(" -> ", "] -> ")
+        text.append(newline)
+    f.seek(0)
+    f.writelines(text)
+    f.truncate()
