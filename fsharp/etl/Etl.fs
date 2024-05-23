cf90b626b53c51ea246125b87cdd0b8fb96bc00b
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-05-23 19:07:10 +0200

    f# etl seq fold map change

--- fsharp/etl/Etl.fs
+++ fsharp/etl/Etl.fs
@@ -1,19 +1,18 @@
 module Etl
 
-open System
-open System.Collections.Generic
+let private addLetterToMap (score: int) (sourceMap: Map<char, int>) (letter: char) : Map<char, int> =
+    Map.change
+        (System.Char.ToLowerInvariant letter)
+        (function
+        | None -> Some score
+        | _ -> failwithf "Letter '%c' is assigned more than one score" letter)
+        sourceMap
 
+let private mapValuesToKey
+    (lettersToScores: Map<char, int>)
+    (KeyValue(score: int, letters: char list))
+    : Map<char, int> =
+    List.fold (addLetterToMap score) lettersToScores letters
 
-let transform (scoresWithLetters: Map<int, char list>) : Map<char, int> =
-    let mutableDictionary = Dictionary<char, int>()
-
-    for KeyValue(score, letterList) in scoresWithLetters do
-        for letter in letterList do
-            let letter' = Char.ToLower letter
-
-            if mutableDictionary.ContainsKey letter' then
-                failwithf "Letter '%c' is assigned more than one score" letter
-
-            mutableDictionary.Add(Char.ToLower letter, score)
-
-    mutableDictionary |> Seq.map (fun kvp -> kvp.Key, kvp.Value) |> Map.ofSeq
+let transform: Map<int, char list> -> Map<char, int> =
+    Seq.fold mapValuesToKey Map.empty
