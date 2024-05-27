35ac05f55d4142e1c2ffe070fb3ac849abab7c16
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-05-27 16:36:18 +0200

    proverb extension

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -1,12 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public static class Proverb
 {
-    public static string[] Recite(string[] subjects) 
-    => subjects.Length == 0 ? subjects
-        : subjects.Zip(subjects.Skip(1))
-        .Select(subjectPair => $"For want of a {subjectPair.First} the {subjectPair.Second} was lost.")
-        .Append($"And all for the want of a {subjects.First()}.")
-        .ToArray();
+    public static string[] Recite(string[] subjects) => subjects.Length > 0 ? subjects.ToProverb().ToArray() : Array.Empty<string>();
+
+    public static IEnumerable<string> ToProverb(this IEnumerable<string> subjects) => subjects
+                    .Zip(subjects.Skip(1))
+                    .Select(subjectPair => $"For want of a {subjectPair.First} the {subjectPair.Second} was lost.")
+                    .Append($"And all for the want of a {subjects.First()}.");
 }
