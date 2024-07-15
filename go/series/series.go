bce5358df665309e9e87ccbf4e0fd77916ef8d47
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-07-15 23:11:59 +0200

    out of bound

--- go/series/series.go
+++ go/series/series.go
@@ -1,29 +1,25 @@
 package series
 
-func All(n int, s string) (out []string) {
+func All(n int, s string) []string {
+	if len(s) < n {
+		return nil
+	}
+
+	out := make([]string, 0, len(s)-n+1)
 
 	for i := 0; i+n <= len(s); i++ {
 		out = append(out, s[i:i+n])
 	}
-
-	return
+	return out
 }
 
-func UnsafeFirst(n int, s string) (out string) {
-
-	if result := All(n, s); len(result) > 0 {
-		out = result[0]
-	}
-
-	return
+func UnsafeFirst(n int, s string) string {
+	return s[:n]
 }
 
-func First(n int, s string) (first string, ok bool) {
-
-	if result := UnsafeFirst(n, s); len(result) > 0 {
-		first = result
-		ok = true
+func First(n int, s string) (string, bool) {
+	if len(s) < n {
+		return "", false
 	}
-
-	return
+	return UnsafeFirst(n, s), true
 }
