c59506b753af3aeabcc56ff64d881e20c80d593f
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-07-21 11:30:49 +0200

    delete _build

--- ocaml/leap/_build/default/leap.ml
+++
@@ -1,5 +0,0 @@
-let leap_year year = 
-    let year_is_divisible_by m = 
-        year mod m == 0 
-    in 
-    year_is_divisible_by 4 && (year_is_divisible_by 400 || not @@ year_is_divisible_by 100)
