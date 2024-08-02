579f14f30e913720afcf587c01a19e8e6bba0f83
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-08-02 08:33:29 +0200

    lisp equality

--- common-lisp/larrys-winning-checker/larrys-winning-checker.lisp
+++ common-lisp/larrys-winning-checker/larrys-winning-checker.lisp
@@ -9,12 +9,22 @@
 
 (in-package :larrys-winning-checker)
 
-(defun make-empty-board () (make-array '(3 3) :initial-element nil))
+(defun make-empty-board () 
+  (make-array '(3 3) :initial-element nil))
 
-(defun make-board-from-list (list) (make-array '(3 3) :initial-contents list))
+(defun make-board-from-list (list) 
+  (make-array '(3 3) :initial-contents list))
 
-(defun all-the-same-p (row-or-col) (and (eq (aref row-or-col 0) (aref row-or-col 1)) (eq (aref row-or-col 0) (aref row-or-col 2))))
+(defun all-the-same-p (row-or-col) 
+  (and  (eq (aref row-or-col 0) (aref row-or-col 1)) 
+        (eq (aref row-or-col 0) (aref row-or-col 2))))
 
-(defun row (board row-num) (vector (aref board row-num 0) (aref board row-num 1) (aref board row-num 2)))
+(defun row (board row-num) 
+  (vector (aref board row-num 0) 
+          (aref board row-num 1) 
+          (aref board row-num 2)))
 
-(defun column (board col-num) (vector (aref board 0 col-num) (aref board 1 col-num) (aref board 2 col-num)))
+(defun column (board col-num) 
+  (vector (aref board 0 col-num) 
+          (aref board 1 col-num) 
+          (aref board 2 col-num)))
