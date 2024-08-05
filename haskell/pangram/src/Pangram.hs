26c5ab71a646ed227916607638bacae4942a2eae
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-08-05 16:39:46 +0200

    pangram

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,7 +1,9 @@
 module Pangram (isPangram) where
 
 import Data.Char
-import qualified Data.Set as Set
 
 isPangram :: String -> Bool
-isPangram text = (Set.fromList . filter isAsciiLower . map toLower) text == Set.fromList ['a' .. 'z']
+isPangram text =
+  let textLower = map toLower text
+      contains ch = elem ch textLower
+   in all contains ['a' .. 'z']
