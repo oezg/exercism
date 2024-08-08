9053e3e44134bcc1c47d19eb066a437421383086
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-08-08 08:02:58 +0200

    mess

--- common-lisp/beer-song/beer-song.lisp
+++ common-lisp/beer-song/beer-song.lisp
@@ -12,23 +12,21 @@
 (defconstant e "~A, ~A.~&")
 (defconstant f "~A bottle~A of beer")
 
-(defun take-or-go (n) (if (= 0 n) d (format nil c (one-it n))))
-(defun one-it (n) (if (= 1 n) "it" "one"))
+(defun one-or-it (n) (if (= 1 n) "it" "one"))
+(defun take-or-go (n) (if (zerop n) d (format nil c (one-or-it n))))
 (defun ending (n) (if (= 1 n) "" "s"))
 (defun stringify (n) (if (zerop n) "no more" (write-to-string (mod n 100))))
-(defun join-lines (line1 line2) (format nil "~A~&~A~&" line1 line2))
-(defun join-verses (verse1 verse2) (format nil "~A~%~A" verse1 verse2))
-(defun line-one (n) (let* ((string-n (stringify n)) (ending-n (ending n)) (hada (format nil a string-n ending-n))) 
-  (format nil e (format nil b hada) hada)))
-(defun line-two (n) (let ((string-n-1 (stringify (- n 1))) (ending-n-1 (ending (- n 1))))
-  (format nil e (take-or-go n) (format nil b (format nil f string-n-1 ending-n-1)))))
+(defun line-one (n) (let ((string-n (stringify n)) (ending-n (ending n))) 
+  (format nil e (format nil b (format nil a string-n ending-n)) (format nil f string-n ending-n))))
+(defun line-two (n)
+  (format nil e (take-or-go n) (format nil b (format nil f (stringify (- n 1)) (ending (- n 1))))))
 
 
 (defun verse (n)
   "Returns a string verse for a given number."
-  (join-lines (line-one n) (line-two n))
-  )
+  (format nil "~A~&~A~&" (line-one n) (line-two n)))
 
 (defun sing (start &optional (end 0))
   "Returns a string of verses for a given range of numbers."
-  )
+  (let* ((numbers (loop for i from start downto end collect i)) (verses (mapcar #'verse numbers)))
+    (format nil "~{~A~%~}" verses)))
