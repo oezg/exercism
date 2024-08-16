1b419f1bfeb0308f903a655cadf700ac4b46887e
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-08-16 16:18:02 +0200

    ~R

--- common-lisp/roman-numerals/roman-numerals.lisp
+++ common-lisp/roman-numerals/roman-numerals.lisp
@@ -4,16 +4,6 @@
 
 (in-package :roman-numerals)
 
-(defparameter *roman* (make-array '(4 10) :initial-contents '(
-  ("" "I" "II" "III" "IV" "V" "VI" "VII" "VIII" "IX") 
-  ("" "X" "XX" "XXX" "XL" "L" "LX" "LXX" "LXXX" "XC") 
-  ("" "C" "CC" "CCC" "CD" "D" "DC" "DCC" "DCCC" "CM") 
-  ("" "M" "MM" "MMM" "" "" "" "" "" "")
-)))
-
-(defun romanize (number)
+(defun romanize (n) 
   "Returns the Roman numeral representation for a given number."
-  (flet ((roman-digit (from-right) 
-      (let ((digit (mod (floor number (expt 10 from-right)) 10))) 
-        (aref *roman* from-right digit))))
-    (format nil "~{~A~}" (mapcar #'roman-digit '(3 2 1 0)))))
+  (format nil "~@R" n))
