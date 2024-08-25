0aa32565ac008e800cda632d009ac9ffc6f0d356
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-08-25 22:46:38 +0200

    optimize for readability

--- go/crypto-square/crypto_square.go
+++ go/crypto-square/crypto_square.go
@@ -4,22 +4,20 @@ package cryptosquare
 import (
 	"math"
 	"strings"
+	"unicode"
 )
 
 // Given an English text, Encode outputs the encoded version of that text.
 func Encode(pt string) string {
-	length := 0
-	normalized := make([]byte, len(pt))
-	for _, v := range []byte(pt) {
-		if 'a' <= v && v <= 'z' || '0' <= v && v <= '9' {
-			normalized[length] = v
-			length++
-		} else if 'A' <= v && v <= 'Z' {
-			normalized[length] = 32 + v
-			length++
+	normalized := make([]rune, 0, len(pt))
+	for _, v := range strings.ToLower(pt) {
+		if unicode.IsLetter(v) || unicode.IsDigit(v) {
+			normalized = append(normalized, v)
 		}
 	}
 
+	// Find the sides of the smallest square/rectangle
+	length := len(normalized)
 	row := int(math.Sqrt(float64(length)))
 	col := row + 1
 	if row*col < length {
@@ -28,26 +26,40 @@ func Encode(pt string) string {
 		col--
 	}
 
-	normalized = normalized[:length]
-
+	// Pad the code with space to fill the rectangle
 	for i := length; i < row*col; i++ {
 		normalized = append(normalized, ' ')
 	}
 
+	// Write the runes in the transposed position
 	sb := strings.Builder{}
 	sb.Grow((row + 1) * col)
-
 	for i := 0; i < col; i++ {
 		for j := 0; j < row; j++ {
-			sb.WriteByte(normalized[j*col+i])
+			sb.WriteRune(normalized[j*col+i])
 		}
 		if i < col-1 {
-			sb.WriteByte(' ')
+			sb.WriteRune(' ')
 		}
 	}
 
 	return sb.String()
 }
+
+// 11. call `ToLower` before iterating on the whole message.
+// BenchmarkEncode-4         197564              5952 ns/op            2536 B/op         47 allocs/op
+
+// 10. call `ToLower` in every case.
+// BenchmarkEncode-4         214290              5539 ns/op            2176 B/op         38 allocs/op
+
+// 9. `append` instead of indexing into `normalized`
+// BenchmarkEncode-4         209914              5058 ns/op            2176 B/op         38 allocs/op
+
+// 8. `unicode` package for filtering
+// BenchmarkEncode-4         237376              4934 ns/op            2176 B/op         38 allocs/op
+
+// 7. rune instead of byte slice and `WriteRune` instead of `WriteByte`
+// BenchmarkEncode-4         269425              4268 ns/op            2176 B/op         38 allocs/op
 
 // 6. index into `normalized` instead of append
 // BenchmarkEncode-4         451284              2304 ns/op             944 B/op         38 allocs/op
