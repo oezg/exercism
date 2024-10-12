6794b25dac0f6c92ed2a7cefa313ed811baf24bc
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-10-13 00:33:06 +0200

    clj

--- clojure/pangram/src/pangram.clj
+++ clojure/pangram/src/pangram.clj
@@ -1,17 +1,8 @@
 (ns pangram (:require [clojure.string :as s]
-                      [clojure.set :as set]
-                      [criterium.core :as crit]))
+                      [clojure.set :as set]))
 
-(def lower-alphabet
-  (map char (range (int \a) (inc (int \z)))))
-
-(def lower-alphabet-set
-  (set lower-alphabet))
-
-(defn pangram2 [sentence]
-  (every? (set (s/lower-case sentence)) lower-alphabet))
+(def ^:private lower-alphabet
+  (set (map char (range (int \a) (inc (int \z))))))
 
 (defn pangram? [sentence]
-  (set/subset? lower-alphabet-set (set (s/lower-case sentence))))
-
-(crit/bench (pangram2 "qwertas"))
+  (set/subset? lower-alphabet (set (s/lower-case sentence))))
