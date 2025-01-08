99515263013a7d716e8195a7b303aa4448b2afa5
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-01-08 17:40:32 +0100

    bash

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-declare -a sounds=([3]=i [5]=a [7]=o)
+declare -a sounds=([3]=Pling [5]=Plang [7]=Plong)
 
 for i in "${!sounds[@]}"; do
-    (($1 % i)) || result+=Pl${sounds[$i]}ng
+    (($1 % i)) || result+=${sounds[$i]}
 done
 
 echo "${result-$1}"
