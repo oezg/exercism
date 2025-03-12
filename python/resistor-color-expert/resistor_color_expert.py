e66a1a299f2b976638ab298949b7d803b3e8446d
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-03-12 14:20:11 +0100

    py resist

--- python/resistor-color-expert/resistor_color_expert.py
+++ python/resistor-color-expert/resistor_color_expert.py
@@ -1,2 +1,72 @@
-def resistor_label(colors):
-    pass
+import enum
+from functools import reduce
+
+
+class ResistorColor(enum.Enum):
+    BLACK = 0
+    BROWN = 1
+    RED = 2
+    ORANGE = 3
+    YELLOW = 4
+    GREEN = 5
+    BLUE = 6
+    VIOLET = 7
+    GREY = 8
+    WHITE = 9
+
+
+class Tolerance(enum.Enum):
+    GREY = 0.05
+    VIOLET = 0.1
+    BLUE = 0.25
+    GREEN = 0.5
+    BROWN = 1
+    RED = 2
+    GOLD = 5
+    SILVER = 10
+
+    def __str__(self) -> str:
+        return f"±{self.value}%"
+
+
+class Prefix(enum.Enum):
+    GIGA = 1_000_000_000
+    MEGA = 1_000_000
+    KILO = 1_000
+    UNIT = 1
+
+    def __str__(self) -> str:
+        match self:
+            case Prefix.UNIT:
+                return ""
+            case _:
+                return self.name.lower()
+
+
+def resistor_label(colors: list[str]) -> str:
+    try:
+        *values, exponent, tolerance = colors
+    except:
+        return label(color_code(colors[0]))
+    return f"{label(resistance_val(colors=values) * multiplier(exponent))} {Tolerance[tolerance.upper()]}"
+
+
+def resistance_val(colors: list[str]) -> int:
+    return sum(
+        10 ** (len(colors) - i) * color_code(color) for i, color in enumerate(colors, 1)
+    )
+
+
+def multiplier(color: str) -> int:
+    return 10 ** color_code(color=color)
+
+
+def color_code(color: str) -> int:
+    return ResistorColor[color.upper()].value
+
+
+def label(resistance: int) -> str:
+    for prefix in Prefix:
+        if resistance >= prefix.value:
+            return f"{(resistance / prefix.value):n} {prefix}ohms"
+    return "0 ohms"
---
+++ python/resistor-color-expert/resistor_color_trio.py
@@ -0,0 +1,45 @@
+import enum
+
+
+class ResistorColor(enum.Enum):
+    BLACK = 0
+    BROWN = 1
+    RED = 2
+    ORANGE = 3
+    YELLOW = 4
+    GREEN = 5
+    BLUE = 6
+    VIOLET = 7
+    GREY = 8
+    WHITE = 9
+
+
+class Prefix(enum.Enum):
+    GIGA = 1_000_000_000
+    MEGA = 1_000_000
+    KILO = 1_000
+    UNIT = 1
+
+    def __str__(self) -> str:
+        match self:
+            case Prefix.UNIT:
+                return ""
+            case _:
+                return self.name.lower()
+
+
+def label(colors: list[str]) -> int:
+    first_band, second_band, third_band, *_ = colors
+    resistance = resistance_value(first_band, second_band, third_band)
+    for prefix in Prefix:
+        if resistance >= prefix.value:
+            return f'{resistance // prefix.value} {prefix}ohms'
+    return '0 ohms'
+
+
+def resistance_value(first_band, second_band, third_band: str) -> int:
+    return (10 * color_code(first_band) + color_code(second_band)) * 10 ** color_code(third_band)
+
+
+def color_code(color: str) -> int:
+    return ResistorColor[color.upper()].value
