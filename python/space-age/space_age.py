0074fe1a8052d0b6198308b8dc5a70c8f1fdcd3d
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-04-08 18:27:57 +0200

    py space

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,29 +1,25 @@
 from collections.abc import Callable
 
+PLANETS: dict[str, float] = {
+    "earth": 1.0,
+    "mercury": 0.2408467,
+    "venus": 0.61519726,
+    "mars": 1.8808158,
+    "jupiter": 11.862615,
+    "saturn": 29.447498,
+    "uranus": 84.016846,
+    "neptune": 164.79132,
+}
+
 
 class SpaceAge:
-    SECONDS: int = 31_557_600  # Earth year in seconds
-    PLANETS: dict[str, float] = {
-        "earth": 1.0,
-        "mercury": 0.2408467,
-        "venus": 0.61519726,
-        "mars": 1.8808158,
-        "jupiter": 11.862615,
-        "saturn": 29.447498,
-        "uranus": 84.016846,
-        "neptune": 164.79132,
-    }
-
     def __init__(self, seconds: int) -> None:
-        self.earth_years = seconds / SpaceAge.SECONDS
+        self.earth_years = seconds / 31_557_600
 
 
-def on_planet(orbit: float) -> Callable[[SpaceAge], int]:
-    def clojure(self: SpaceAge) -> int:
-        return round(self.earth_years / orbit, 2)
-
-    return clojure
+def on_planet(orbit: float) -> Callable[[SpaceAge], float]:
+    return lambda self: round(self.earth_years / orbit, 2)
 
 
-for planet, orbit in SpaceAge.PLANETS.items():
+for planet, orbit in PLANETS.items():
     setattr(SpaceAge, f"on_{planet}", on_planet(orbit))
