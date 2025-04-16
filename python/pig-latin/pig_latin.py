b736cde2278d434d79f3432e0e106ebe30a6e94e
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-04-16 16:20:27 +0200

    py pig

---
+++ python/pig-latin/pig_latin.py
@@ -0,0 +1,30 @@
+import re
+
+
+def translate(text: str) -> str:
+    """
+    Translate text from English to Pig Latin.
+
+    :param text: english
+    :return: pig latin
+    """
+    return " ".join(map(pig_latin, text.split()))
+
+
+def pig_latin(text: str) -> str:
+    return rotate(text) + "ay"
+
+
+def rotate(text: str) -> str:
+    if text.startswith(("xr", "yt")):
+        return text
+    if text.startswith("y"):
+        return rotate(text[1:] + "y")
+    initial_consonants, rest = split_vowel(text)
+    if rest.startswith("u") and initial_consonants.endswith("q"):
+        return rest[1:] + initial_consonants + "u"
+    return rest + initial_consonants
+
+
+def split_vowel(text: str) -> list[str]:
+    return re.split(r"(?=[aeiouy])", text, maxsplit=1)
