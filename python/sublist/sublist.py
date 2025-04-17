ebe4a3717a9fcb94821a5138906455ad5511475b
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-04-17 15:45:36 +0200

    py sublist

--- python/sublist/sublist.py
+++ python/sublist/sublist.py
@@ -1,22 +1,19 @@
-"""
-This exercise stub and the test suite contain several enumerated constants.
-
-Enumerated constants can be done with a NAME assigned to an arbitrary,
-but unique value. An integer is traditionally used because it’s memory
-efficient.
-It is a common practice to export both constants and functions that work with
-those constants (ex. the constants in the os, subprocess and re modules).
-
-You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
-"""
-
-# Possible sublist categories.
-# Change the values as you see fit.
-SUBLIST = None
-SUPERLIST = None
-EQUAL = None
-UNEQUAL = None
+UNEQUAL, SUPERLIST, SUBLIST, EQUAL = 0, 1, 2, 3
 
 
-def sublist(list_one, list_two):
-    pass
+def sublist(a: list, b: list) -> int:
+    """
+    Given any two lists A and B, determine whether
+    * A is equal to B,
+    * A is a sublist of B,
+    * A is a superlist of B,
+    * otherwise they are unequal.
+    """
+    return int(f"{contains(a, b)}{contains(b, a)}", 2)
+
+
+def contains(a: list, b: list) -> int:
+    """
+    List A is a sublist of B if B contains a contiguous sub-sequence of values equal to A
+    """
+    return int(any(b[i : i + len(a)] == a for i in range(len(b) - len(a) + 1)))
