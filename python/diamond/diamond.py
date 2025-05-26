cce235d6c90ef48817b2174a735e38040212fa80
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-05-26 15:17:37 +0200

    py diamond

--- python/diamond/diamond.py
+++ python/diamond/diamond.py
@@ -4,10 +4,12 @@ import string
 def rows(letter: str) -> list[str]:
     idx = string.ascii_uppercase.index(letter)
     letters = list(string.ascii_uppercase)[: idx + 1]
+    vertical_letters = letters[:-1] + letters[::-1]
+    horizontal_letters = letters[::-1] + letters[1:]
     return [
         "".join(
             horizontal if horizontal == vertical else " "
-            for horizontal in letters[::-1] + letters[1:]
+            for horizontal in horizontal_letters
         )
-        for vertical in letters[:-1] + letters[::-1]
+        for vertical in vertical_letters
     ]
