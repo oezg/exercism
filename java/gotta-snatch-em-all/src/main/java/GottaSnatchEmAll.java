c7efbe433e445e8b97e0cb89b12f73c3095bc22a
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-05-28 17:58:11 +0200

    java map

--- java/gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
+++ java/gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
@@ -1,4 +1,3 @@
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -6,34 +5,40 @@ import java.util.stream.Stream;
 
 class GottaSnatchEmAll {
 
-    static Set<String> newCollection(List<String> cards) {
-        return Set.copyOf(cards);
-    }
+  static Set<String> newCollection(List<String> cards) {
+    return Set.copyOf(cards);
+  }
 
-    static boolean addCard(String card, Set<String> collection) {
-        return collection.add(card);
-    }
+  static boolean addCard(String card, Set<String> collection) {
+    return collection.add(card);
+  }
 
-    static boolean canTrade(Set<String> myCollection, Set<String> theirCollection) {
-        if (myCollection.isEmpty()) {
-            return false;
+  static boolean canTrade(Set<String> myCollection, Set<String> theirCollection) {
+    for (String card : theirCollection) {
+      if (!myCollection.contains(card)) {
+        for (String mycard : myCollection) {
+          if (!theirCollection.contains(mycard)) {
+            return true;
+          }
         }
-        return !myCollection.containsAll(theirCollection);
+      }
     }
+    return false;
+  }
 
-    static Set<String> commonCards(List<Set<String>> collections) {
-        return collections.stream().reduce((acc, set) -> intersection(acc, set)).orElse(Set.of());
-    }
+  static Set<String> commonCards(List<Set<String>> collections) {
+    return collections.stream().reduce((acc, set) -> intersection(acc, set)).orElse(Set.of());
+  }
 
-    static Set<String> allCards(List<Set<String>> collections) {
-        return collections.stream().reduce((acc, set) -> union(acc, set)).orElse(Set.of());
-    }
+  static Set<String> allCards(List<Set<String>> collections) {
+    return collections.stream().reduce((acc, set) -> union(acc, set)).orElse(Set.of());
+  }
 
-    private static Set<String> union(Set<String> set1, Set<String> set2) {
-        return Stream.concat(set1.stream(), set2.stream()).collect(Collectors.toSet());
-    }
+  private static Set<String> union(Set<String> set1, Set<String> set2) {
+    return Stream.concat(set1.stream(), set2.stream()).collect(Collectors.toSet());
+  }
 
-    private static Set<String> intersection(Set<String> set1, Set<String> set2) {
-        return set1.stream().filter(set2::contains).collect(Collectors.toSet());
-    }
+  private static Set<String> intersection(Set<String> set1, Set<String> set2) {
+    return set1.stream().filter(set2::contains).collect(Collectors.toSet());
+  }
 }
---
+++ java/international-calling-connoisseur/src/main/java/DialingCodes.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DialingCodes {
+  private Map<Integer, String> code2country = new HashMap<>();
+  private Map<String, Integer> country2code = new HashMap<>();
+
+  public Map<Integer, String> getCodes() {
+    return code2country;
+  }
+
+  public void setDialingCode(Integer code, String country) {
+    code2country.put(code, country);
+    country2code.put(country, code);
+  }
+
+  public String getCountry(Integer code) {
+    return code2country.get(code);
+  }
+
+  public void addNewDialingCode(Integer code, String country) {
+    if (code2country.containsKey(code) || country2code.containsKey(country)) {
+      return;
+    }
+    setDialingCode(code, country);
+  }
+
+  public Integer findDialingCode(String country) {
+    return country2code.get(country);
+  }
+
+  public void updateCountryDialingCode(Integer code, String country) {
+    if (findDialingCode(country) == null) {
+      setDialingCode(code, country);
+    }
+    code2country.remove(findDialingCode(country));
+  }
+}
