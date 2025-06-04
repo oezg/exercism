7a13466f6c6319b88265219aed7f090900e3eeb1
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-06-04 23:21:40 +0200

    lazy seq all

--- clojure/strain/src/strain.clj
+++ clojure/strain/src/strain.clj
@@ -3,13 +3,13 @@
 (defn retain
   "Keeps the items in coll for which (pred item) returns true."
   [pred coll]
-  (if (seq coll)
-    (lazy-seq
+  (lazy-seq
+   (if (seq coll)
      (let [[head & tail] coll]
        (if (pred head)
          (cons head (retain pred tail))
-         (retain pred tail))))
-    '()))
+         (retain pred tail)))
+     '())))
 
 (defn discard
   "Removes the items in coll for which (pred item) returns true."
