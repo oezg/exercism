56b479afaf975cd0cd7954d4175859af0696d62c
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-06-11 17:39:53 +0200

    java micro blog

--- java/micro-blog/src/main/java/MicroBlog.java
+++ java/micro-blog/src/main/java/MicroBlog.java
@@ -1,16 +1,13 @@
+import java.text.BreakIterator;
+
 class MicroBlog {
 
-  private static final int MAX_POST_UNICODE_POINTS = 5;
+  private static final int POST_LENGTH = 5;
 
   public String truncate(String input) {
-    return fromCodePoints(takeCodePoints(input, MAX_POST_UNICODE_POINTS));
-  }
-
-  private int[] takeCodePoints(String input, int count) {
-    return input.codePoints().limit(count).toArray();
-  }
-
-  private String fromCodePoints(int[] codePoints) {
-    return new String(codePoints, 0, Integer.min(codePoints.length, MAX_POST_UNICODE_POINTS));
+    BreakIterator charIterator = BreakIterator.getCharacterInstance();
+    charIterator.setText(input);
+    int boundary = charIterator.next(POST_LENGTH);
+    return boundary == BreakIterator.DONE ? input : input.substring(0, boundary);
   }
 }
