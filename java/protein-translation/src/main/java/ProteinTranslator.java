be557b5a1c69f2a67b8f9f266c23a8b0b9a9a9af
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-06-12 17:18:40 +0200

    prot trans

--- java/protein-translation/src/main/java/ProteinTranslator.java
+++ java/protein-translation/src/main/java/ProteinTranslator.java
@@ -1,8 +1,51 @@
+import static java.util.Map.entry;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 class ProteinTranslator {
 
-    List<String> translate(String rnaSequence) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+  private static final String STOP = "STOP";
+  private static final int CODON_SIZE = 3;
+
+  private static final Map<String, String> aminoAcids =
+      Map.ofEntries(
+          entry("AUG", "Methionine"),
+          entry("UUU", "Phenylalanine"),
+          entry("UUC", "Phenylalanine"),
+          entry("UUA", "Leucine"),
+          entry("UUG", "Leucine"),
+          entry("UCU", "Serine"),
+          entry("UCC", "Serine"),
+          entry("UCA", "Serine"),
+          entry("UCG", "Serine"),
+          entry("UAC", "Tyrosine"),
+          entry("UAU", "Tyrosine"),
+          entry("UGU", "Cysteine"),
+          entry("UGC", "Cysteine"),
+          entry("UGG", "Tryptophan"),
+          entry("UAA", STOP),
+          entry("UAG", STOP),
+          entry("UGA", STOP));
+
+  List<String> translate(String rnaSequence) {
+    List<String> protein = new ArrayList<>();
+    for (int i = 0; i < rnaSequence.length(); i += CODON_SIZE) {
+      try {
+        String codon = rnaSequence.substring(i, i + CODON_SIZE);
+        String aminoAcid = aminoAcids.get(codon);
+        if (aminoAcid == STOP) {
+          break;
+        }
+        if (aminoAcid == null) {
+          throw new IllegalArgumentException("Invalid codon");
+        }
+        protein.add(aminoAcid);
+      } catch (StringIndexOutOfBoundsException e) {
+        throw new IllegalArgumentException("Invalid codon");
+      }
     }
+    return protein;
+  }
 }
