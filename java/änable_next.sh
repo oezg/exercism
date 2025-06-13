3359d0235f9486b7fb9d8a13245c58f276c45359
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-06-13 16:10:31 +0200

    script to disable java tests

---
+++ java/änable_next.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+DISABLE='@Disabled("Remove to run test")'
+IMPORT="import org.junit.jupiter.api.Disabled;"
+
+enable_next_test() {
+  find "$PWD/src/test/java" \
+    -name "*Test.java" \
+    -exec sed -i "0,/$DISABLE/{/$DISABLE/d;}" {} \; \
+    -quit
+}
+
+enable_all_tests() {
+  find "$PWD/src/test/java" \
+    -name "*Test.java" \
+    -exec sed -i -e "/$IMPORT/d" -e "/$DISABLE/d" {} \; \
+    -quit
+}
+
+if [ $# -eq 0 ]; then
+  enable_next_test
+else
+  enable_all_tests
+fi
