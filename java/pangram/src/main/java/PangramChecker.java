62c0545061f145448956203e3773e17678275662
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-06-27 09:54:58 +0200

    pangram

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,17 +1,12 @@
-import java.util.Collections;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.function.IntPredicate;
 
 public class PangramChecker {
-  private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
-  private static final Set<Character> letters = setOfCharacters(alphabet);
+  private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
   public boolean isPangram(String input) {
-    return setOfCharacters(input.toLowerCase()).containsAll(letters);
-  }
+    IntPredicate foundInInputCaseInsensitive =
+        c -> !(input.indexOf(c) < 0 && input.indexOf(Character.toUpperCase(c)) < 0);
 
-  private static Set<Character> setOfCharacters(String input) {
-    return Collections.unmodifiableSet(
-        input.chars().mapToObj(c -> (char) c).collect(Collectors.toSet()));
+    return ALPHABET.chars().allMatch(foundInInputCaseInsensitive);
   }
 }
