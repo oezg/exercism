25097519c6c7285ae41e9396afa8de5a5b724626
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-07-18 00:24:15 +0200

    lasagna

---
+++ elixir/lasagna/lib/lasagna.ex
@@ -0,0 +1,7 @@
+defmodule Lasagna do
+  def expected_minutes_in_oven(), do: 40
+  def remaining_minutes_in_oven(minutes), do: expected_minutes_in_oven() - minutes
+  def preparation_time_in_minutes(layers), do: layers * 2
+  def total_time_in_minutes(layers, minutes), do: preparation_time_in_minutes(layers) + minutes
+  def alarm(), do: "Ding!"
+end
