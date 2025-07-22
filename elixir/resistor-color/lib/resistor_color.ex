2ce36b6044cbe112c6f78c5193e623d2d5d13b10
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-07-22 13:49:00 +0200

    FunctionClauseError

--- elixir/resistor-color/lib/resistor_color.ex
+++ elixir/resistor-color/lib/resistor_color.ex
@@ -1,20 +1,16 @@
 defmodule ResistorColor do
-  @color_codes %{
-    black: 0,
-    brown: 1,
-    red: 2,
-    orange: 3,
-    yellow: 4,
-    green: 5,
-    blue: 6,
-    violet: 7,
-    grey: 8,
-    white: 9
-  }
-
   @doc """
   Return the value of a color band
   """
-  @spec code(atom) :: integer()
-  def code(color), do: Map.fetch!(@color_codes, color)
+  @spec code(atom) :: integer
+  def code(:black), do: 0
+  def code(:brown), do: 1
+  def code(:red), do: 2
+  def code(:orange), do: 3
+  def code(:yellow), do: 4
+  def code(:green), do: 5
+  def code(:blue), do: 6
+  def code(:violet), do: 7
+  def code(:grey), do: 8
+  def code(:white), do: 9
 end
