fddd62b3ca0220c8c8e33aca8cbef144425552ff
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-10-06 16:53:16 +0200

    java diamonf

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,11 +1,12 @@
 import java.util.List;
+import java.util.function.IntPredicate;
 import java.util.stream.Stream;
 
 class HandshakeCalculator {
 
   List<Signal> calculateHandshake(int number) {
-    var result =
-        Stream.of(Signal.values()).filter(signal -> (number >> signal.ordinal() & 1) == 1).toList();
-    return (number >> 4 & 1) == 1 ? result.reversed() : result;
+    IntPredicate isSet = n -> (number >> n & 1) == 1;
+    List<Signal> result = Stream.of(Signal.values()).filter(s -> isSet.test(s.ordinal())).toList();
+    return isSet.test(4) ? result.reversed() : result;
   }
 }
