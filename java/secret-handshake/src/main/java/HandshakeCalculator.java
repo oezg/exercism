886a1018f04cff075b7562133228b7695cbb963a
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-10-09 10:41:07 +0200

    handshake

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,12 +1,12 @@
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.IntPredicate;
 import java.util.stream.Stream;
 
 class HandshakeCalculator {
 
   List<Signal> calculateHandshake(int number) {
-    IntPredicate isSet = n -> (number >> n & 1) == 1;
-    List<Signal> result = Stream.of(Signal.values()).filter(s -> isSet.test(s.ordinal())).toList();
-    return isSet.test(4) ? result.reversed() : result;
+    return Stream.of(Signal.values())
+        .filter(s -> s.yes(number))
+        .collect(ArrayList::new, Signal::action, List::addAll);
   }
 }
--- java/secret-handshake/src/main/java/Signal.java
+++ java/secret-handshake/src/main/java/Signal.java
@@ -1,5 +1,23 @@
+import java.util.Collections;
+import java.util.List;
+
 enum Signal {
+  WINK,
+  DOUBLE_BLINK,
+  CLOSE_YOUR_EYES,
+  JUMP,
+  REVERSE;
 
-    WINK, DOUBLE_BLINK, CLOSE_YOUR_EYES, JUMP
+  public boolean yes(int number) {
+    return (number >> ordinal() & 1) == 1;
+  }
 
+  public static List<Signal> action(List<Signal> list, Signal signal) {
+    if (signal == REVERSE) {
+      Collections.reverse(list);
+    } else {
+      list.add(signal);
+    }
+    return list;
+  }
 }
