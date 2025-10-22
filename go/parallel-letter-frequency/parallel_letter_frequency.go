bb9c58352b288af7472ba3d2638342034963caab
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-10-22 17:25:37 +0200

    acronym

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -20,23 +20,36 @@ func worker(text string, fmp chan FreqMap, wg *sync.WaitGroup) {
 	fmp <- Frequency(text)
 }
 
+func sth(text string, runeChan chan rune, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for _, r := range text {
+		runeChan <- r
+	}
+}
+
 // ConcurrentFrequency counts the frequency of each rune in the given strings,
 // by making use of concurrency.
 func ConcurrentFrequency(texts []string) FreqMap {
 	var wg sync.WaitGroup
 	result := make(FreqMap)
-	freqMapChan := make(chan FreqMap, len(texts))
+	runeChan := make(chan rune)
+	// freqMapChan := make(chan FreqMap, len(texts))
 	wg.Add(len(texts))
 	for _, text := range texts {
-		go worker(text, freqMapChan, &wg)
+		// go worker(text, freqMapChan, &wg)
+		go sth(text, runeChan, &wg)
 	}
 	wg.Wait()
-	close(freqMapChan)
+	// close(freqMapChan)
+	close(runeChan)
 
-	for freqMap := range freqMapChan {
-		for r, i := range freqMap {
-			result[r] += i
-		}
+	// for freqMap := range freqMapChan {
+	// 	for r, i := range freqMap {
+	// 		result[r] += i
+	// 	}
+	// }
+	for r := range runeChan {
+		result[r]++
 	}
 	return result
 }
--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -2,5 +2,4 @@ import re
 
 
 def abbreviate(words: str) -> str:
-    trim_underscore = [frag.lstrip("_") for frag in re.split(r"[ -]", words)]
-    return "".join(frag[0].upper() for frag in trim_underscore if frag != "")
+    return "".join(frag.lstrip("_")[0].upper() for frag in re.split(r"[ -]+", words))
