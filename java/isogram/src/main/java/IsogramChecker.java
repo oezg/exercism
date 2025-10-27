e5a92a7d0924fac6a4cdc90e3a05837dee8e7341
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-10-27 14:21:06 +0100

    java

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,16 +1,12 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class IsogramChecker {
 
-  private static final Set<Character> allowed = Set.of('-', ' ');
-
   boolean isIsogram(String phrase) {
-    Set<Character> visited = new HashSet<>();
-    for (Character character : phrase.toLowerCase().toCharArray()) {
-      if (visited.contains(character)) return false;
-
-      if (!allowed.contains(character)) visited.add(character);
+    var sieve = new boolean[26];
+    for (char character : phrase.toLowerCase().toCharArray()) {
+      if (character == ' ' || character == '-') continue;
+      var index = character - 'a';
+      if (sieve[index]) return false;
+      sieve[index] = true;
     }
     return true;
   }
