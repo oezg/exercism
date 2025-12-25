4c177a7810f0fbb68eed3d6dcd70d525b5639ef2
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-12-25 01:31:47 +0100

    java

--- java/nucleotide-count/src/main/java/NucleotideCounter.java
+++ java/nucleotide-count/src/main/java/NucleotideCounter.java
@@ -1,26 +1,41 @@
-import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 final class NucleotideCounter {
-  private final Map<Character, Integer> counts =
-      new HashMap<>(Map.of('A', 0, 'C', 0, 'G', 0, 'T', 0));
+  private static final Map<Character, Integer> origin;
+
+  static {
+    origin = Map.of('A', 0, 'C', 0, 'G', 0, 'T', 0);
+  }
+
+  private final Map<Character, Integer> counts;
 
   NucleotideCounter(String sequence) {
-    counts.putAll(
-        sequence
-            .chars()
-            .mapToObj(cp -> Character.valueOf((char) cp))
-            .collect(
-                Collectors.groupingBy(
-                    c -> {
-                      if (counts.containsKey(c)) return c;
-                      throw new IllegalArgumentException();
-                    },
-                    Collectors.summingInt(nucleotide -> 1))));
+    counts = mergeWith(countNucleotides(sequence));
   }
 
   Map<Character, Integer> nucleotideCounts() {
     return counts;
+  }
+
+  private static Map<Character, Integer> mergeWith(Map<Character, Integer> nucleotidesInSequence) {
+    return Stream.of(origin, nucleotidesInSequence)
+        .flatMap(m -> m.entrySet().stream())
+        .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (v1, v2) -> v2));
+  }
+
+  private static Map<Character, Integer> countNucleotides(String sequence) {
+    return sequence
+        .chars()
+        .mapToObj(cp -> (char) cp)
+        .peek(NucleotideCounter::validate)
+        .collect(
+            Collectors.groupingBy(Function.identity(), Collectors.summingInt(nucleotide -> 1)));
+  }
+
+  private static void validate(Character c) {
+    if (!origin.containsKey(c)) throw new IllegalArgumentException("Invalid nucleotide: " + c);
   }
 }
