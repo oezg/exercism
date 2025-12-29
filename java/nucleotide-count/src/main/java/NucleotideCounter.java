4fa3e13587f31031c0bbb39221669da04b84015a
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-12-29 15:57:07 +0100

    java

--- java/nucleotide-count/src/main/java/NucleotideCounter.java
+++ java/nucleotide-count/src/main/java/NucleotideCounter.java
@@ -1,38 +1,32 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 final class NucleotideCounter {
-  private static final Map<Character, Integer> origin;
-
-  static {
-    origin = Map.of('A', 0, 'C', 0, 'G', 0, 'T', 0);
-  }
+  private static final Map<Character, Integer> origin = Map.of('A', 0, 'C', 0, 'G', 0, 'T', 0);
 
   private final Map<Character, Integer> counts;
 
   NucleotideCounter(String sequence) {
-    counts = mergeWith(countNucleotides(sequence));
+    counts = countNucleotides(sequence);
   }
 
   Map<Character, Integer> nucleotideCounts() {
-    return counts;
-  }
-
-  private static Map<Character, Integer> mergeWith(Map<Character, Integer> nucleotidesInSequence) {
-    return Stream.of(origin, nucleotidesInSequence)
-        .flatMap(m -> m.entrySet().stream())
-        .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (v1, v2) -> v2));
+    return Map.copyOf(counts);
   }
 
   private static Map<Character, Integer> countNucleotides(String sequence) {
-    return sequence
-        .chars()
-        .mapToObj(cp -> (char) cp)
-        .peek(NucleotideCounter::validate)
-        .collect(
-            Collectors.groupingBy(Function.identity(), Collectors.summingInt(nucleotide -> 1)));
+    var interim = new HashMap<>(origin);
+    interim.putAll(
+        sequence
+            .chars()
+            .mapToObj(cp -> (char) cp)
+            .peek(NucleotideCounter::validate)
+            .collect(
+                Collectors.groupingBy(
+                    Function.identity(), Collectors.summingInt(nucleotide -> 1))));
+    return interim;
   }
 
   private static void validate(Character c) {
