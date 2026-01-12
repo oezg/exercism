a557be8a6d34902ed61636d8e031ce9a16884da6
Özgür Yıldırım <ozguryenihayat@gmail.com>
2026-01-12 17:43:57 +0100

    java

--- java/killer-sudoku-helper/src/main/java/KillerSudokuHelper.java
+++ java/killer-sudoku-helper/src/main/java/KillerSudokuHelper.java
@@ -1,28 +1,29 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class KillerSudokuHelper {
 
   List<List<Integer>> combinationsInCage(Integer cageSum, Integer cageSize, List<Integer> exclude) {
-    return combinations(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9), cageSize)
-        .filter(
-            cmb ->
-                cmb.stream().reduce(0, Integer::sum) == cageSum
-                    && !cmb.stream().anyMatch(i -> exclude.contains(i)))
-        .toList();
+    var include = IntStream.rangeClosed(1, 9).filter(n -> !exclude.contains(n)).boxed().toList();
+    return combinationsWithSum(include, cageSize, cageSum, List.of()).toList();
   }
 
   List<List<Integer>> combinationsInCage(Integer cageSum, Integer cageSize) {
     return combinationsInCage(cageSum, cageSize, List.of());
   }
 
-  static <T> Stream<List<T>> combinations(List<T> sequence, int size) {
-    if (size == 0) return Stream.of(List.of());
-    if (sequence.isEmpty()) return Stream.empty();
-    List<T> tail = sequence.subList(1, sequence.size());
-    return Stream.concat(
-        combinations(tail, size - 1)
-            .map(list -> Stream.concat(Stream.of(sequence.get(0)), list.stream()).toList()),
-        combinations(tail, size));
+  static Stream<List<Integer>> combinationsWithSum(
+      List<Integer> input, int k, int subtotal, List<Integer> acc) {
+    if (k == 0 && subtotal == 0) return Stream.of(acc);
+    if (k == 0 || input.isEmpty()) return Stream.empty();
+    var head = input.getFirst();
+    var tail = input.stream().skip(1).toList();
+    var newacc = new ArrayList<>(acc);
+    newacc.add(head);
+    var withHead = combinationsWithSum(tail, k - 1, subtotal - head, newacc);
+    var withoutHead = combinationsWithSum(tail, k, subtotal, acc);
+    return Stream.concat(withHead, withoutHead);
   }
 }
