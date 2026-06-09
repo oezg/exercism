abfdbe89d13289719bca27de227a742a995f6bae
Özgür Yıldırım <ozguryenihayat@gmail.com>
2026-06-09 10:23:41 +0200

    protein&rna

---
+++ kotlin/protein-translation/src/main/kotlin/ProteinTranslation.kt
@@ -0,0 +1,26 @@
+fun translate(rna: String?): List<String> = 
+    rnaSeq(rna ?: "").map { it.name }.toList()
+
+fun rnaSeq(rna: String): Sequence<AminoAcid> =
+    rna.chunkedSequence(3) {
+        when (it) {
+            "AUG" -> TranslationResult.Continue(AminoAcid.Methionine)
+            "UUU", "UUC" -> TranslationResult.Continue(AminoAcid.Phenylalanine)
+            "UUA", "UUG" -> TranslationResult.Continue(AminoAcid.Leucine)
+            "UCU", "UCC", "UCA", "UCG" -> TranslationResult.Continue(AminoAcid.Serine)
+            "UAU", "UAC" -> TranslationResult.Continue(AminoAcid.Tyrosine)
+            "UGU", "UGC" -> TranslationResult.Continue(AminoAcid.Cysteine)
+            "UGG" -> TranslationResult.Continue(AminoAcid.Tryptophan)
+            "UAA", "UAG", "UGA" -> TranslationResult.Stop
+            else -> throw IllegalArgumentException("Invalid codon")
+        }
+    }.takeWhile { it is TranslationResult.Continue }.mapNotNull { (it as? TranslationResult.Continue)?.aminoAcid }
+
+sealed interface TranslationResult {
+    object Stop : TranslationResult
+    data class Continue(val aminoAcid: AminoAcid) : TranslationResult 
+}
+
+enum class AminoAcid {
+    Methionine, Phenylalanine, Leucine, Serine, Tyrosine, Cysteine, Tryptophan
+}
---
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -0,0 +1,11 @@
+fun transcribeToRna(dna: String): String =
+    dna.map {
+        when(it) {
+            'G' -> 'C'
+            'C' -> 'G'
+            'T' -> 'A'
+            'A' -> 'U'
+            else -> throw IllegalArgumentException("Invalid nucleotide: $it")
+        }
+    }.joinToString("")
+
