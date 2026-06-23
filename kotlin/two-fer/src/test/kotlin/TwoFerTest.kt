21184201aa667c353a1a2e7b0f5b155248135f52
Özgür Yıldırım <ozguryenihayat@gmail.com>
2026-06-23 10:56:18 +0200

    twofer

---
+++ kotlin/two-fer/src/test/kotlin/TwoFerTest.kt
@@ -0,0 +1,30 @@
+import org.junit.Ignore
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class TwoFerTest {
+
+    @Test
+    fun noNameGiven() {
+        assertEquals("One for you, one for me.", twofer())
+    }
+
+//    @Ignore
+    @Test
+    fun aNameGiven() {
+        assertEquals("One for Alice, one for me.", twofer("Alice"))
+    }
+
+//    @Ignore
+    @Test
+    fun anotherNameGiven() {
+        assertEquals("One for Bob, one for me.", twofer("Bob"))
+    }
+
+//    @Ignore
+    @Test
+    fun emptyStringGiven() {
+        assertEquals("One for , one for me.", twofer(""))
+    }
+
+}
