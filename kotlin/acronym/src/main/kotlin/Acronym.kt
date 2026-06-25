df88d3ecd53090b696398445061c3e33b57c4234
Özgür Yıldırım <ozguryenihayat@gmail.com>
2026-06-25 14:12:21 +0200

    acronym

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -1,13 +1,6 @@
 object Acronym {
-    fun generate(phrase: String): String = buildString {
-        var wordStart = true
-        for (c in phrase) {
-            if (wordStart && (c in 'a'..'z' || c in 'A'..'Z')) {
-                append(c)
-                wordStart = false
-            } else if (!wordStart && (c == ' ' || c == '-')) {
-                wordStart = true
-            }
-        }
-    }.uppercase()
+    fun generate(phrase: String): String =
+        phrase.splitToSequence(' ', '-')
+            .mapNotNull { word -> word.firstOrNull { it.isLetter() } }
+            .joinToString("") { it.uppercase() }
 }
